955b15d1b73f78b01eb9b1ad1c47f9cbb1575c09
Rouslan Solomakhin <rouslan@chromium.org>
2016-06-17 09:44:22 -0700

    Add ToT tests

---
+++ tot/pr/multi/pr.js
@@ -0,0 +1,129 @@
+/* global done:false */
+/* global error:false */
+/* global PaymentRequest:false */
+
+/**
+ * Updates the details based on the selected shipping option.
+ * @param {object} details - The current details to update.
+ * @param {string} shippingOption - The shipping option selected by user.
+ * @return {object} The updated details.
+ */
+function updateDetails(details, shippingOption) {
+  var selectedShippingOption;
+  var otherShippingOption;
+  if (shippingOption === 'standard') {
+    selectedShippingOption = details.shippingOptions[0];
+    otherShippingOption = details.shippingOptions[1];
+    details.total.amount.value = '55.00';
+  } else {
+    selectedShippingOption = details.shippingOptions[1];
+    otherShippingOption = details.shippingOptions[0];
+    details.total.amount.value = '67.00';
+  }
+  if (details.displayItems.length === 2) {
+    details.displayItems.splice(1, 0, selectedShippingOption);
+  } else {
+    details.displayItems.splice(1, 1, selectedShippingOption);
+  }
+  selectedShippingOption.selected = true;
+  otherShippingOption.selected = false;
+  return details;
+}
+
+/**
+ * Launches payment request that provides multiple shipping options worldwide,
+ * regardless of the shipping address.
+ */
+function onBuyClicked() {  // eslint-disable-line no-unused-vars
+  var supportedInstruments = [
+    {
+      supportedMethods: ['https://android.com/pay'],
+      data: {
+        merchantId: '123456',
+        allowedCardNetworks: ['AMEX', 'MASTERCARD', 'VISA'],
+        paymentMethodTokenizationParameters: {
+          tokenizationType: 'GATEWAY_TOKEN',
+          parameters: {
+            'gateway': 'stripe',
+            'stripe:publishableKey': 'pk_test_VKUbaXb3LHE7GdxyOBMNwXqa',
+            'stripe:version': '2015-10-16 (latest)'
+          }
+        }
+      }
+    },
+    {
+      supportedMethods: [
+        'visa', 'mastercard', 'amex', 'discover', 'maestro', 'diners', 'jcb',
+        'unionpay'
+      ]
+    }
+  ];
+
+  var details = {
+    total: {label: 'Donation', amount: {currency: 'USD', value: '55.00'}},
+    displayItems: [
+      {
+        label: 'Original donation amount',
+        amount: {currency: 'USD', value: '65.00'}
+      },
+      {
+        label: 'Friends and family discount',
+        amount: {currency: 'USD', value: '-10.00'}
+      }
+    ],
+    shippingOptions: [
+      {
+        id: 'standard',
+        label: 'Standard shipping',
+        amount: {currency: 'USD', value: '0.00'},
+        selected: true
+      },
+      {
+        id: 'express',
+        label: 'Express shipping',
+        amount: {currency: 'USD', value: '12.00'}
+      }
+    ]
+  };
+
+  var options = {requestShipping: true};
+
+  if (!window.PaymentRequest) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  try {
+    var request = new PaymentRequest(supportedInstruments, details, options);
+
+    request.addEventListener('shippingoptionchange', function(e) {
+      e.updateWith(new Promise(function(resolve) {
+        resolve(updateDetails(details, request.shippingOption));
+      }));
+    });
+
+    request.show()
+        .then(function(instrumentResponse) {
+          window.setTimeout(function() {
+            instrumentResponse.complete('success')
+                .then(function() {
+                  done(
+                      'Thank you!', instrumentResponse.shippingAddress,
+                      request.shippingOption, instrumentResponse.methodName,
+                      instrumentResponse.details,
+                      instrumentResponse.totalAmount,
+                      instrumentResponse.payerEmail,
+                      instrumentResponse.payerPhone);
+                })
+                .catch(function(err) {
+                  error(err.message);
+                });
+          }, 2000);
+        })
+        .catch(function(err) {
+          error(err.message);
+        });
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+  }
+}
---
+++ tot/pr/single/pr.js
@@ -0,0 +1,90 @@
+/* global done:false */
+/* global error:false */
+/* global PaymentRequest:false */
+
+/**
+ * Launches payment request that provides free shipping worldwide.
+ */
+function onBuyClicked() {  // eslint-disable-line no-unused-vars
+  var supportedInstruments = [
+    {
+      supportedMethods: ['https://android.com/pay'],
+      data: {
+        merchantId: '123456',
+        allowedCardNetworks: ['AMEX', 'MASTERCARD', 'VISA'],
+        paymentMethodTokenizationParameters: {
+          tokenizationType: 'GATEWAY_TOKEN',
+          parameters: {
+            'gateway': 'stripe',
+            'stripe:publishableKey': 'pk_test_VKUbaXb3LHE7GdxyOBMNwXqa',
+            'stripe:version': '2015-10-16 (latest)'
+          }
+        }
+      }
+    },
+    {
+      supportedMethods: [
+        'visa', 'mastercard', 'amex', 'discover', 'maestro', 'diners', 'jcb',
+        'unionpay'
+      ]
+    }
+  ];
+
+  var details = {
+    total: {label: 'Donation', amount: {currency: 'USD', value: '55.00'}},
+    displayItems: [
+      {
+        label: 'Original donation amount',
+        amount: {currency: 'USD', value: '65.00'}
+      },
+      {
+        label: 'Friends and family discount',
+        amount: {currency: 'USD', value: '-10.00'}
+      },
+      {
+        label: 'Free worldwide shipping',
+        amount: {currency: 'USD', value: '0.00'}
+      }
+    ],
+    shippingOptions: [{
+      id: 'freeShippingOption',
+      label: 'Free worldwide shipping',
+      amount: {currency: 'USD', value: '0.00'},
+      selected: true
+    }]
+  };
+
+  var options = {requestShipping: true};
+
+  if (!window.PaymentRequest) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  try {
+    var request = new PaymentRequest(supportedInstruments, details, options);
+    request.show()
+        .then(function(instrumentResponse) {
+          window.setTimeout(function() {
+            instrumentResponse.complete('success')
+                .then(function() {
+                  done(
+                      'Thank you!', instrumentResponse.shippingAddress,
+                      request.shippingOption, instrumentResponse.methodName,
+                      instrumentResponse.details,
+                      instrumentResponse.totalAmount,
+                      instrumentResponse.payerEmail,
+                      instrumentResponse.payerPhone);
+                })
+                .catch(function(err) {
+                  error(err.message);
+                });
+          }, 2000);
+        })
+        .catch(function(err) {
+          error(err.message);
+        });
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+  }
+}
