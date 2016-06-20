5b7fa8b915fa7dc3df25438e23f92955b2ba3e1b
Rouslan Solomakhin <rouslan@chromium.org>
2016-06-20 10:10:47 -0700

    Add contact info testing pages.

---
+++ pr/contact/pr.js
@@ -0,0 +1,81 @@
+/* global done:false */
+/* global error:false */
+/* global PaymentRequest:false */
+
+/**
+ * Launches payment request that provides free shipping worldwide.
+ */
+function onBuyClicked() {  // eslint-disable-line no-unused-vars
+  var supportedInstruments = [
+    {
+      supportedMethods: ['https://android.com/pay'],
+      data: {
+        merchantId: '123456',
+        allowedCardNetworks: ['AMEX', 'MASTERCARD', 'VISA'],
+        paymentMethodTokenizationParameters: {
+          tokenizationType: 'GATEWAY_TOKEN',
+          parameters: {
+            'gateway': 'stripe',
+            'stripe:publishableKey': 'pk_test_VKUbaXb3LHE7GdxyOBMNwXqa',
+            'stripe:version': '2015-10-16 (latest)'
+          }
+        }
+      }
+    },
+    {
+      supportedMethods: [
+        'visa', 'mastercard', 'amex', 'discover', 'maestro', 'diners', 'jcb',
+        'unionpay'
+      ]
+    }
+  ];
+
+  var details = {
+    total: {label: 'Donation', amount: {currency: 'USD', value: '55.00'}},
+    displayItems: [
+      {
+        label: 'Original donation amount',
+        amount: {currency: 'USD', value: '65.00'}
+      },
+      {
+        label: 'Friends and family discount',
+        amount: {currency: 'USD', value: '-10.00'}
+      }
+    ]
+  };
+
+  var options = {
+    requestPayerEmail: true,
+    requestPayerPhone: true
+  };
+
+  if (!window.PaymentRequest) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  try {
+    var request = new PaymentRequest(supportedInstruments, details, options);
+    request.show()
+        .then(function(instrumentResponse) {
+          window.setTimeout(function() {
+            instrumentResponse.complete('success')
+                .then(function() {
+                  done(
+                      'Thank you!', undefined, undefined,
+                      instrumentResponse.methodName, instrumentResponse.details,
+                      undefined, instrumentResponse.payerEmail,
+                      instrumentResponse.payerPhone);
+                })
+                .catch(function(err) {
+                  error(err.message);
+                });
+          }, 2000);
+        })
+        .catch(function(err) {
+          error(err.message);
+        });
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+  }
+}
