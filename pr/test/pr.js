0c531b6a5c5ee3277593827b9e7ac4282d54413d
Rouslan Solomakhin <rouslan@chromium.org>
2016-08-26 14:36:59 -0700

    Add test env

---
+++ pr/test/pr.js
@@ -0,0 +1,72 @@
+/* global done:false */
+/* global error:false */
+/* global PaymentRequest:false */
+
+/**
+ * Launches payment request for Android Pay with TEST env.
+ */
+function onBuyClicked() {  // eslint-disable-line no-unused-vars
+  var supportedInstruments = [
+    {
+      supportedMethods: ['https://android.com/pay'],
+      data: {
+        environment: 'TEST',
+        merchantName: 'Rouslan Solomakhin',
+        merchantId: '0',
+        allowedCardNetworks: ['AMEX', 'MASTERCARD', 'VISA', 'DISCOVER'],
+        paymentMethodTokenizationParameters: {
+          tokenizationType: 'GATEWAY_TOKEN',
+          parameters: {
+            'gateway': 'stripe',
+            'stripe:publishableKey': '<DOES_NOT_MATTER_IN_TEST>',
+            'stripe:version': '2016-07-06'
+          }
+        }
+      }
+    }
+  ];
+
+  var details = {
+    total: {label: 'Donation', amount: {currency: 'USD', value: '55.00'}},
+    displayItems: [
+      {
+        label: 'Original donation amount',
+        amount: {currency: 'USD', value: '65.00'}
+      },
+      {
+        label: 'Friends and family discount',
+        amount: {currency: 'USD', value: '-10.00'}
+      }
+    ]
+  };
+
+  var options = {
+    requestPayerEmail: true
+  };
+
+  if (!window.PaymentRequest) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  try {
+    var request = new PaymentRequest(supportedInstruments, details, options);
+    request.show()
+        .then(function(instrumentResponse) {
+          window.setTimeout(function() {
+            instrumentResponse.complete('success')
+                .then(function() {
+                  done('Thank you!', instrumentResponse);
+                })
+                .catch(function(err) {
+                  error(err);
+                });
+          }, 2000);
+        })
+        .catch(function(err) {
+          error(err);
+        });
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+  }
+}
