1da4c2b8e522a5e43143d5f6998e7338e1227b3b
Rouslan Solomakhin <rouslan@chromium.org>
2016-11-09 11:15:54 -0500

    Re-initialize payment request object after UI was closed.

--- pr/pr.js
+++ pr/pr.js
@@ -2,56 +2,64 @@
 /* global error:false */
 /* global PaymentRequest:false */
 
-var supportedInstruments = [
-  {
-    supportedMethods: ['https://android.com/pay'],
-    data: {
-      merchantName: 'Rouslan Solomakhin',
-      merchantId: '00184145120947117657',
-      allowedCardNetworks: ['AMEX', 'MASTERCARD', 'VISA', 'DISCOVER'],
-      paymentMethodTokenizationParameters: {
-        tokenizationType: 'GATEWAY_TOKEN',
-        parameters: {
-          'gateway': 'stripe',
-          'stripe:publishableKey': 'pk_live_lNk21zqKM2BENZENh3rzCUgo',
-          'stripe:version': '2016-07-06'
+/**
+ * Initializes the payment request object.
+ */
+function buildPaymentRequest() {
+  if (!window.PaymentRequest) {
+    return null;
+  }
+
+  var supportedInstruments = [
+    {
+      supportedMethods: ['https://android.com/pay'],
+      data: {
+        merchantName: 'Rouslan Solomakhin',
+        merchantId: '00184145120947117657',
+        allowedCardNetworks: ['AMEX', 'MASTERCARD', 'VISA', 'DISCOVER'],
+        paymentMethodTokenizationParameters: {
+          tokenizationType: 'GATEWAY_TOKEN',
+          parameters: {
+            'gateway': 'stripe',
+            'stripe:publishableKey': 'pk_live_lNk21zqKM2BENZENh3rzCUgo',
+            'stripe:version': '2016-07-06'
+          }
         }
       }
-    }
-  },
-  {
-    supportedMethods: [
-      'unionpay', 'visa', 'mastercard', 'amex', 'discover', 'diners', 'jcb'
-    ]
-  }
-];
-
-var details = {
-  total: {label: 'Donation', amount: {currency: 'USD', value: '55.00'}},
-  displayItems: [
-    {
-      label: 'Original donation amount',
-      amount: {currency: 'USD', value: '65.00'}
     },
     {
-      label: 'Friends and family discount',
-      amount: {currency: 'USD', value: '-10.00'}
+      supportedMethods: [
+        'unionpay', 'visa', 'mastercard', 'amex', 'discover', 'diners', 'jcb'
+      ]
     }
-  ],
-  modifiers: [{
-    supportedMethods: ['visa'],
-    total: {label: 'Donation', amount: {currency: 'USD', value: '45.00'}},
-    additionalDisplayItems: [{
-      label: 'VISA discount', amount: {currency: 'USD', value: '-10.00'}
-    }],
-    data: {
-      discountProgramParticipantId: '86328764873265'
-    }
-  }]
-};
+  ];
 
-var request = null;
-if (window.PaymentRequest) {
+  var details = {
+    total: {label: 'Donation', amount: {currency: 'USD', value: '55.00'}},
+    displayItems: [
+      {
+        label: 'Original donation amount',
+        amount: {currency: 'USD', value: '65.00'}
+      },
+      {
+        label: 'Friends and family discount',
+        amount: {currency: 'USD', value: '-10.00'}
+      }
+    ],
+    modifiers: [{
+      supportedMethods: ['visa'],
+      total: {label: 'Donation', amount: {currency: 'USD', value: '45.00'}},
+      additionalDisplayItems: [{
+        label: 'VISA discount', amount: {currency: 'USD', value: '-10.00'}
+      }],
+      data: {
+        discountProgramParticipantId: '86328764873265'
+      }
+    }]
+  };
+
+  var request = null;
+
   try {
     request = new PaymentRequest(supportedInstruments, details);
     if (request.canMakeActivePayment) {
@@ -64,7 +72,11 @@ if (window.PaymentRequest) {
   } catch (e) {
     error('Developer mistake: \'' + e + '\'');
   }
+
+  return request;
 }
+
+var request = buildPaymentRequest();
 
 /**
  * Launches payment request that does not require shipping.
@@ -82,16 +94,20 @@ function onBuyClicked() {  // eslint-disable-line no-unused-vars
             instrumentResponse.complete('success')
                 .then(function() {
                   done('Thank you!', instrumentResponse);
+                  request = buildPaymentRequest();
                 })
                 .catch(function(err) {
                   error(err);
+                  request = buildPaymentRequest();
                 });
           }, 2000);
         })
         .catch(function(err) {
           error(err);
+          request = buildPaymentRequest();
         });
   } catch (e) {
     error('Developer mistake: \'' + e + '\'');
+    request = buildPaymentRequest();
   }
 }
