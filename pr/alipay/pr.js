e599b83b850b03f6a01c0a71f49ae7daa25bab05
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2017-02-08 11:10:56 -0500

    Alipay

--- pr/alipay/pr.js
+++ pr/alipay/pr.js
@@ -1,39 +1,35 @@
-/* global done:false */
-/* global error:false */
-/* global PaymentRequest:false */
-
 /**
  * Launches payment request for AliPay.
  */
-function onBuyClicked() {  // eslint-disable-line no-unused-vars
-  var supportedInstruments = [
-    {
-      // Browser downloads https://alipay.com/payment-app.json to verify
-      // identity of the Android app that handles this payment.
-      supportedMethods: ['https://alipay.com'],
-      data: {
-        // Browser does not parse this data and passes it directly to AliPay app.
-        partner: '2088001159940003',
-        _input_charset: 'UTF-8',
-        sign_type: 'RSA',
-        sign: 'e5815a4556db338ed237f7d3fd222184',
-        out_trade_no: 'PARTNER_TRANS_ID_113'
-      }
-    }
-  ];
+function onBuyClicked() { // eslint-disable-line no-unused-vars
+  let supportedInstruments = [{
+    supportedMethods: ['https://www.alipay.com/webpay'],
+    data: {
+      orderInfo: 'biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.01%22%2C%22subject%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E4%B8%9C%E8%A5%BF%3B%22%2C%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%3B%22%2C%22out_trade_no%22%3A%221486526346841%22%7D&sign=mRs8nGetdSFZ6a48mWSkODioWi81glyGKq7vDp2DQ0aQ2aVz8C5rcbfxTggZBG9QvQyjBYkPPjEAfk1RNfqxGDVxNVe0amKNAZwiC%2Bs4loQ3WvliiovCtPWJBkP59EdeTBVgT0jbfY6Hl4hRHEyGM%2BIm7R3gkpHSkM2cU8BrnwI2Apn1gkn%2FlqLAWC%2BeHMpT%2Bjr%2BEv5RKCPyIVEA85y96JDfDOvjX0KEn9OjQFvUkJ0%2BvnM8r5AbLQTdlF6fXZ1Jnza1uo3LQ%2BiT6j0FCzvys9%2BxuV1kLGfRvVywQmUotnXg68Fejm93unzarKkpceeGrVRc51%2FU2M17j2PJGttkdQ%3D%3D&method=alipay.trade.app.pay&charset=utf-8&version=1.0&app_id=2014100900013222&timestamp=2017-02-08+11%3A59%3A06&sign_type=RSA2', // eslint-disable-line max-len
+    },
+  }];
 
-  var details = {
-    total: {label: 'Donation', amount: {currency: 'USD', value: '55.00'}},
-    displayItems: [
-      {
-        label: 'Original donation amount',
-        amount: {currency: 'USD', value: '65.00'}
+  let details = {
+    total: {
+      label: 'Donation',
+      amount: {
+        currency: 'USD',
+        value: '55.00',
       },
-      {
-        label: 'Friends and family discount',
-        amount: {currency: 'USD', value: '-10.00'}
-      }
-    ]
+    },
+    displayItems: [{
+      label: 'Original donation amount',
+      amount: {
+        currency: 'USD',
+        value: '65.00',
+      },
+    }, {
+      label: 'Friends and family discount',
+      amount: {
+        currency: 'USD',
+        value: '-10.00',
+      },
+    }],
   };
 
   if (!window.PaymentRequest) {
@@ -42,22 +38,22 @@ function onBuyClicked() {  // eslint-disable-line no-unused-vars
   }
 
   try {
-    var request = new PaymentRequest(supportedInstruments, details);
+    let request = new PaymentRequest(supportedInstruments, details);
     request.show()
-        .then(function(instrumentResponse) {
-          window.setTimeout(function() {
-            instrumentResponse.complete('success')
-                .then(function() {
-                  done('Thank you!', instrumentResponse);
-                })
-                .catch(function(err) {
-                  error(err);
-                });
-          }, 2000);
-        })
-        .catch(function(err) {
-          error(err);
-        });
+      .then(function(instrumentResponse) {
+        window.setTimeout(function() {
+          instrumentResponse.complete('success')
+            .then(function() {
+              done('Thank you!', instrumentResponse);
+            })
+            .catch(function(err) {
+              error(err);
+            });
+        }, 2000);
+      })
+      .catch(function(err) {
+        error(err);
+      });
   } catch (e) {
     error('Developer mistake: \'' + e.message + '\'');
   }
