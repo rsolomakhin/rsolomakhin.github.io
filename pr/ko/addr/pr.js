46f3cb7103307fab74d4fba64efa3ac125dc62ff
Rouslan Solomakhin <rouslan@chromium.org>
2017-02-16 11:44:28 -0500

    Add failure test cases

---
+++ pr/ko/addr/pr.js
@@ -0,0 +1,89 @@
+/* global done:false */
+/* global error:false */
+/* global PaymentRequest:false */
+
+/**
+ * Launches payment request that provides multiple shipping options worldwide,
+ * regardless of the shipping address.
+ */
+function onBuyClicked() {  // eslint-disable-line no-unused-vars
+  var supportedInstruments = [
+    {
+      supportedMethods: ['https://android.com/pay'],
+      data: {
+        merchantName: 'Rouslan Solomakhin',
+        merchantId: '00184145120947117657',
+        allowedCardNetworks: ['AMEX', 'MASTERCARD', 'VISA', 'DISCOVER'],
+        paymentMethodTokenizationParameters: {
+          tokenizationType: 'GATEWAY_TOKEN',
+          parameters: {
+            'gateway': 'stripe',
+            'stripe:publishableKey': 'pk_live_lNk21zqKM2BENZENh3rzCUgo',
+            'stripe:version': '2016-07-06'
+          }
+        }
+      }
+    },
+    {
+      supportedMethods: [
+        'visa', 'mastercard', 'amex', 'discover', 'diners', 'jcb', 'unionpay'
+      ]
+    }
+  ];
+
+  var details = {
+    total: {label: 'Donation', amount: {currency: 'USD', value: '55.00'}},
+    displayItems: [
+      {
+        label: 'Original donation amount',
+        amount: {currency: 'USD', value: '65.00'}
+      },
+      {
+        label: 'Friends and family discount',
+        amount: {currency: 'USD', value: '-10.00'}
+      }
+    ],
+    shippingOptions: [
+      {
+        id: 'standard',
+        label: 'Standard shipping',
+        amount: {currency: 'USD', value: '0.00'},
+        selected: true
+      },
+      {
+        id: 'express',
+        label: 'Express shipping',
+        amount: {currency: 'USD', value: '12.00'}
+      }
+    ]
+  };
+
+  var options = {requestShipping: true};
+
+  if (!window.PaymentRequest) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  try {
+    var request = new PaymentRequest(supportedInstruments, details, options);
+    // No shipping address or option change listeners.
+    request.show()
+        .then(function(instrumentResponse) {
+          window.setTimeout(function() {
+            instrumentResponse.complete('success')
+                .then(function() {
+                  done('Thank you!', instrumentResponse);
+                })
+                .catch(function(err) {
+                  error(err);
+                });
+          }, 2000);
+        })
+        .catch(function(err) {
+          error(err);
+        });
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+  }
+}
