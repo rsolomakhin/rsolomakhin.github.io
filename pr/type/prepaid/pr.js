d2d2770981cf3faa9112cd4733018a7b3b0c39a9
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2017-04-26 17:28:47 -0400

    Add credit, debit, prepaid test case.

---
+++ pr/type/prepaid/pr.js
@@ -0,0 +1,76 @@
+/* global done:false */
+/* global error:false */
+
+/**
+ * Initializes the payment request object.
+ * @return {PaymentRequest} A new instance of payment request object.
+ */
+function buildPaymentRequest() {
+  if (!window.PaymentRequest) {
+    return null;
+  }
+
+  const supportedInstruments = [{
+    supportedMethods: ['basic-card'],
+    data: {
+      supportedTypes: ['prepaid'],
+    },
+  }];
+
+  const details = {
+    total: {
+      label: 'Total',
+      amount: {
+        currency: 'USD',
+        value: '1.00',
+      },
+    },
+  };
+
+  let request = null;
+
+  try {
+    request = new PaymentRequest(supportedInstruments, details);
+    if (request.canMakePayment) {
+      request.canMakePayment().then(function(result) {
+        info(result ? 'Can make payment' : 'Cannot make payment');
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+  } catch (e) {
+    error('Developer mistake: \'' + e + '\'');
+  }
+
+  return request;
+}
+
+let request = buildPaymentRequest();
+
+/**
+ * Launches payment request for credit cards.
+ */
+function onBuyClicked() { // eslint-disable-line no-unused-vars
+  if (!window.PaymentRequest || !request) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  try {
+    request.show()
+      .then(function(instrumentResponse) {
+        instrumentResponse.complete().then(function() {
+          done(JSON.stringify(instrumentResponse, undefined, 2));
+        }).catch(function(err) {
+          error(err);
+        });
+      })
+      .catch(function(err) {
+        error(err);
+      });
+  } catch (e) {
+    error('Developer mistake: \'' + e + '\'');
+  }
+
+  request = buildPaymentRequest();
+}
