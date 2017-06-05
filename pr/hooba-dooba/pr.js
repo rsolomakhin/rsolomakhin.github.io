80bb82f816c471835b363249b31add303f882535
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2017-06-05 11:25:12 -0400

    Fix syntax

--- pr/hooba-dooba/pr.js
+++ pr/hooba-dooba/pr.js
@@ -7,72 +7,73 @@ function buildPaymentRequest() {
   }
 
   var supportedInstruments = [{
-      supportedMethods: [
-        'basic-card',
-      ],
-      data: {
-        supportedNetworks: ['hooba-dooba'],
-      }];
-
-    var details = {
-      total: {
-        label: 'Tots',
-        amount: {
-          currency: 'USD',
-          value: '1.00'
-        }
-      }
-    };
-
-    var request = null;
-
-    try {
-      request = new PaymentRequest(supportedInstruments, details);
-      if (request.canMakePayment) {
-        request.canMakePayment().then(function(result) {
-          info(result ? "Can make payment" : "Cannot make payment");
-        }).catch(function(err) {
-          error(err);
-        });
-      }
-    } catch (e) {
-      error('Developer mistake: \'' + e + '\'');
+    supportedMethods: [
+      'basic-card',
+    ],
+    data: {
+      supportedNetworks: ['hooba-dooba'],
     }
+  }];
 
-    return request;
+  var details = {
+    total: {
+      label: 'Tots',
+      amount: {
+        currency: 'USD',
+        value: '1.00'
+      }
+    }
+  };
+
+  var request = null;
+
+  try {
+    request = new PaymentRequest(supportedInstruments, details);
+    if (request.canMakePayment) {
+      request.canMakePayment().then(function(result) {
+        info(result ? "Can make payment" : "Cannot make payment");
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+  } catch (e) {
+    error('Developer mistake: \'' + e + '\'');
   }
 
-  var request = buildPaymentRequest();
+  return request;
+}
 
-  /**
-   * Launches payment request for credit cards.
-   */
-  function onBuyClicked() { // eslint-disable-line no-unused-vars
-    if (!window.PaymentRequest || !request) {
-      error('PaymentRequest API is not supported.');
-      return;
-    }
+var request = buildPaymentRequest();
 
-    try {
-      request.show()
-        .then(function(instrumentResponse) {
-          window.setTimeout(function() {
-            instrumentResponse.complete('success')
-              .then(function() {
-                done('This is a demo website. No payment will be processed.', instrumentResponse);
-              })
-              .catch(function(err) {
-                error(err);
-                request = buildPaymentRequest();
-              });
-          }, 2000);
-        })
-        .catch(function(err) {
-          error(err);
-          request = buildPaymentRequest();
-        });
-    } catch (e) {
-      error('Developer mistake: \'' + e + '\'');
-      request = buildPaymentRequest();
-    }
+/**
+ * Launches payment request for credit cards.
+ */
+function onBuyClicked() { // eslint-disable-line no-unused-vars
+  if (!window.PaymentRequest || !request) {
+    error('PaymentRequest API is not supported.');
+    return;
   }
+
+  try {
+    request.show()
+      .then(function(instrumentResponse) {
+        window.setTimeout(function() {
+          instrumentResponse.complete('success')
+            .then(function() {
+              done('This is a demo website. No payment will be processed.', instrumentResponse);
+            })
+            .catch(function(err) {
+              error(err);
+              request = buildPaymentRequest();
+            });
+        }, 2000);
+      })
+      .catch(function(err) {
+        error(err);
+        request = buildPaymentRequest();
+      });
+  } catch (e) {
+    error('Developer mistake: \'' + e + '\'');
+    request = buildPaymentRequest();
+  }
+}
