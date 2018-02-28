e833cdef6588be01c5483801bc658ae3a3ba6a8e
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2018-02-28 11:24:56 -0500

    Add failure tests.

---
+++ pr/ko/timeout-show-promise/pr.js
@@ -0,0 +1,107 @@
+/* global done:false */
+/* global error:false */
+/* global PaymentRequest:false */
+
+/**
+ * Initializes the payment request object.
+ */
+function buildPaymentRequest() {
+  if (!window.PaymentRequest) {
+    return null;
+  }
+
+  var supportedInstruments = [
+    {
+      supportedMethods: ['basic-card'],
+    },
+  ];
+
+  var details = {
+    total: {
+      label: 'Donation',
+      amount: {
+        currency: 'USD',
+        value: '1.00',
+        pending: true,
+      },
+    },
+  };
+
+  var request = null;
+
+  try {
+    request = new PaymentRequest(supportedInstruments, details);
+    if (request.canMakePayment) {
+      request
+        .canMakePayment()
+        .then(function(result) {
+          info(result ? 'Can make payment' : 'Cannot make payment');
+        })
+        .catch(function(err) {
+          error(err);
+        });
+    }
+  } catch (e) {
+    error("Developer mistake: '" + e + "'");
+  }
+
+  return request;
+}
+
+var request = buildPaymentRequest();
+
+/**
+ * Launches payment request for credit cards.
+ */
+function onBuyClicked() {
+  // eslint-disable-line no-unused-vars
+  if (!window.PaymentRequest || !request) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  try {
+    request
+      .show(
+        new Promise(function(resolve) {
+          info('Calculating final price...');
+          window.setTimeout(function() {
+            info('The final price is $2.00 USD.');
+            resolve({
+              total: {
+                label: 'Donation',
+                amount: {
+                  currency: 'USD',
+                  value: '2.00',
+                  pending: false,
+                },
+              },
+            });
+          }, 11000); // Longer than the 10 second timeout.
+        }),
+      )
+      .then(function(instrumentResponse) {
+        window.setTimeout(function() {
+          instrumentResponse
+            .complete('success')
+            .then(function() {
+              done(
+                'This is a demo website. No payment will be processed.',
+                instrumentResponse,
+              );
+            })
+            .catch(function(err) {
+              error(err);
+              request = buildPaymentRequest();
+            });
+        }, 2000);
+      })
+      .catch(function(err) {
+        error(err);
+        request = buildPaymentRequest();
+      });
+  } catch (e) {
+    error("Developer mistake: '" + e + "'");
+    request = buildPaymentRequest();
+  }
+}
