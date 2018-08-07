5e04092dbf2344225a242de80bf4a4be404fd3d2
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2018-08-07 13:05:12 -0400

    Add skip-UI

---
+++ pr/skip-ui/pr.js
@@ -0,0 +1,81 @@
+/**
+ * Initializes the payment request object.
+ * @return {PaymentRequest} The payment request object.
+ */
+function buildPaymentRequest() {
+  if (!window.PaymentRequest) {
+    return null;
+  }
+
+  const supportedInstruments = [{
+    supportedMethods: 'https://bobpay.xyz/pay'
+  }
+  ];
+
+  const details = {
+    total: {
+      label: 'Total',
+      amount: {
+        currency: 'USD',
+        value: '1.00',
+      },
+    },
+  };
+
+  let request = null;
+
+  try {
+    request = new PaymentRequest(supportedInstruments, details);
+    if (request.canMakePayment) {
+      request.canMakePayment().then(function(result) {
+        info(result ? 'Can make payment' : 'Cannot make payment');
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+  }
+
+  return request;
+}
+
+let request = buildPaymentRequest();
+
+/**
+ * Handles the response from PaymentRequest.show().
+ */
+function handlePaymentResponse(response) {
+  window.setTimeout(function() {
+    response.complete('success')
+      .then(function() {
+        done('This is a demo website. No payment will be processed.', response);
+      })
+      .catch(function(err) {
+        error(err);
+        request = buildPaymentRequest();
+      });
+  }, 500);
+}
+
+/**
+ * Launches payment request for Bob Pay.
+ */
+function onBuyClicked() { // eslint-disable-line no-unused-vars
+  if (!window.PaymentRequest || !request) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  try {
+    request.show()
+      .then(handlePaymentResponse)
+      .catch(function(err) {
+        error(err);
+        request = buildPaymentRequest();
+      });
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+    request = buildPaymentRequest();
+  }
+}
