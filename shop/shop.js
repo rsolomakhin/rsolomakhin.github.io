a58c8b7f0d4f3c957453b35dcda77a4e13bc845c
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2018-08-31 15:01:48 -0400

    js-beautify

--- shop/shop.js
+++ shop/shop.js
@@ -1,5 +1,5 @@
 if ((window.location.host === 'rsolomakhin.github.io') &&
-    (window.location.protocol !== 'https:')) {
+  (window.location.protocol !== 'https:')) {
   window.location.protocol = 'https:';
 }
 
@@ -25,49 +25,55 @@ window.onload = () => {
   };
 
   syncButton.onclick = (e) => {
-    navigator.credentials.get({password : true, mediation : 'required'})
-        .then(cred => {
-          if (cred) {
-            sessionIdentifierParagraph.innerHTML = cred.id;
-            numberOfWidgets.value = cred.password;
-            credential = cred;
-            syncButton.style.display = 'none';
-            forgetButton.style.display = 'inline';
-          } else {
+    navigator.credentials.get({
+        password: true,
+        mediation: 'required'
+      })
+      .then(cred => {
+        if (cred) {
+          sessionIdentifierParagraph.innerHTML = cred.id;
+          numberOfWidgets.value = cred.password;
+          credential = cred;
+          syncButton.style.display = 'none';
+          forgetButton.style.display = 'inline';
+        } else {
+          credential = new PasswordCredential({
             // In production, generate a unique ID on the server.
-            const username = 'light-weight-user-session-' +
-                             Math.floor(Math.random() * 1000000000000000000);
-            sessionIdentifierParagraph.innerHTML = username;
-            credential = new PasswordCredential({
-              id : username,
-              name : 'Light Weight User Session',
-              // In production, UI state should be saved in cookies and on the
-              // server instead of in this password field. This password field
-              // can be a large random number similar to the username.
-              password : numberOfWidgets.value
+            id: 'light-weight-user-session-' +
+              Math.floor(Math.random() * 1000000000000000000),
+            name: 'Light Weight User Session',
+            // In production, UI state should be saved in cookies and on the
+            // server instead of in this password field. This password field
+            // can be a large random number.
+            password: numberOfWidgets.value
+          });
+          navigator.credentials.store(credential)
+            .then(() => {
+              sessionIdentifierParagraph.innerHTML = credential.id;
+              syncButton.style.display = 'none';
+              forgetButton.style.display = 'inline';
+            })
+            .catch(error => {
+              errorsParagraph.innerHTML = error;
             });
-            navigator.credentials.store(credential)
-                .then(() => {
-                  syncButton.style.display = 'none';
-                  forgetButton.style.display = 'inline';
-                })
-                .catch(error => { errorsParagraph.innerHTML = error; });
-          }
-        })
-        .catch(error => { errorsParagraph.innerHTML = error; });
+        }
+      })
+      .catch(error => {
+        errorsParagraph.innerHTML = error;
+      });
 
   };
 
   forgetButton.onclick =
-      (e) => {
-        credential = null;
-        sessionIdentifierParagraph.innerHTML = '';
-        if (navigator.credentials.preventSilentAccess) {
-          navigator.credentials.preventSilentAccess();
-        }
-        forgetButton.style.display = 'none';
-        syncButton.style.display = 'inline';
-      };
+    (e) => {
+      credential = null;
+      sessionIdentifierParagraph.innerHTML = '';
+      if (navigator.credentials.preventSilentAccess) {
+        navigator.credentials.preventSilentAccess();
+      }
+      forgetButton.style.display = 'none';
+      syncButton.style.display = 'inline';
+    };
 
   // In production, save the UI state in a cookie and on the server to make it
   // stateful.
