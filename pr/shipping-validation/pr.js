23671dc15fe4e291c1494d4ad4b92cb587e5a3a0
Marcos Cáceres <mcaceres@mozilla.com>
2018-10-05 16:23:43 +1000

    Simplify test case

--- pr/shipping-validation/pr.js
+++ pr/shipping-validation/pr.js
@@ -1,40 +1,35 @@
-/* global done:false */
-/* global error:false */
-/* global PaymentRequest:false */
-
-/**
- * Initializes the payment request object.
- */
-async function buildPaymentRequest() {
-  const supportedInstruments = [
+const supportedInstruments = [
+  {
+    supportedMethods: "basic-card",
+  },
+];
+const details = {
+  total: {
+    label: "Total",
+    amount: {
+      currency: "USD",
+      value: "1.00",
+    },
+  },
+  shippingOptions: [
     {
-      supportedMethods: 'basic-card',
-    },
-  ];
-  const details = {
-    total: {
-      label: 'Total',
+      id: "freeShippingOption",
+      label: "Free shipping",
       amount: {
-        currency: 'USD',
-        value: '1.00',
+        currency: "USD",
+        value: "0.00",
       },
+      selected: true,
     },
-    shippingOptions: [{
-      id: 'freeShippingOption',
-      label: 'Free shipping',
-      amount: {
-        currency: 'USD',
-        value: '0.00'
-      },
-      selected: true
-    }]
-  };
-  const options = {requestShipping: true};
+  ],
+};
+
+async function buildPaymentRequest() {
+  const options = { requestShipping: true };
   const request = new PaymentRequest(supportedInstruments, details, options);
-  info(`${await request.canMakePayment() ? "Can" : "Cannot"} make payment.`);
+  info(`${(await request.canMakePayment()) ? "Can" : "Cannot"} make payment.`);
   return request;
 }
-
 /**
  * Launches payment request for credit cards.
  */
@@ -43,16 +38,22 @@ async function onBuyClicked() {
     const request = await buildPaymentRequest();
     const response = await request.show();
     await validateResponse(response);
-  } catch(err) {
+    await response.complete("success");
+  } catch (err) {
     error(`Developer mistake: '${err}'`);
     throw err;
   }
-  await response.complete('success');
 }
 
 async function validateResponse(response) {
-  if (response.shippingAddress.postalCode !== "12345") {
-    await response.retry({ shippingAddress: { postalCode: "The postal code should be 12345." }});
-  }
-  await validateResponse(response)
+  const {
+    shippingAddress: { postalCode },
+  } = response;
+
+  if (postalCode === "12345") return; // valid!
+
+  await response.retry({
+    shippingAddress: { postalCode: "The postal code should be 12345." },
+  });
+  await validateResponse(response);
 }
