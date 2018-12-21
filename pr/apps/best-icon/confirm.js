99c981c8a17e888ae99219c7babb459263979e8f
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2018-12-21 12:03:00 -0500

    Fix typo

--- pr/apps/best-icon/confirm.js
+++ pr/apps/best-icon/confirm.js
@@ -1,6 +1,6 @@
 const button = document.getElementById('confirm');
 const pleasewait = document.getElementById('pleasewait');
-.addEventListener('click', (evt) => {
+button.addEventListener('click', (evt) => {
   button.style.display = 'none';
   pleasewait.style.display = 'block';
   navigator.serviceWorker.controller.postMessage('confirm');
