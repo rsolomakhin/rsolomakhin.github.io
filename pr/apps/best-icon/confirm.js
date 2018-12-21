32c2cbf29abfa86cfaccf46736305becf60b5174
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2018-12-21 11:59:48 -0500

    More interaction confirmation button.

--- pr/apps/best-icon/confirm.js
+++ pr/apps/best-icon/confirm.js
@@ -1,3 +1,7 @@
-document.getElementById('confirm').addEventListener('click', (evt) => {
+const button = document.getElementById('confirm');
+const pleasewait = document.getElementById('pleasewait');
+.addEventListener('click', (evt) => {
+  button.style.display = 'none';
+  pleasewait.style.display = 'block';
   navigator.serviceWorker.controller.postMessage('confirm');
 });
