fa55112c9c7af6268d23f38dd7ce9057e9d02e24
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2019-04-15 10:55:47 -0400

    Correctly feature-detect

--- pr/pwg/pr.js
+++ pr/pwg/pr.js
@@ -54,7 +54,7 @@ function buildPaymentRequest() {
       });
     }
 
-    if (request.onpaymentmethodchange) {
+    if (request.onpaymentmethodchange !== undefined) {
       info('Will print out payment method change event details here.');
       request.addEventListener('paymentmethodchange', (evt) => {
         info('Payment method change event: ' + JSON.stringify({'methodName': evt.methodName, 'methodDetails': evt.methodDetails}, undefined, 2));
