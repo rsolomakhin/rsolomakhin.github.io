d39641af971e0afb363391846032268e89688dda
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2019-04-18 11:58:37 -0400

    Save event instead of function

--- pr/apps/pmc/app.js
+++ pr/apps/pmc/app.js
@@ -3,7 +3,7 @@ self.importScripts('data.js');
 // TODO: Keep state in the window instead, because a single service worker
 // can talk to multiple windows.
 
-self.changePaymentMethod = null;
+self.paymentRequestEvent = null;
 self.resolver = null;
 self.client = null;
 self.response = response1;
@@ -12,22 +12,22 @@ self.addEventListener('message', (evt) => {
   if (evt.data === 'confirm' && self.resolver !== null) {
     self.resolver(response);
     self.resolver = null;
-  } else if (evt.data === 'change-method-1' && self.changePaymentMethod !== null) {
+  } else if (evt.data === 'change-method-1' && self.paymentRequestEvent !== null && self.paymentRequestEvent.changePaymentMethod) {
     response = response1;
-    self.changePaymentMethod(response.methodName, redact(response)).then((paymentMethodChangeResponse) => {
+    self.paymentRequestEvent.changePaymentMethod(response.methodName, redact(response)).then((paymentMethodChangeResponse) => {
       self.client.postMessage(paymentMethodChangeResponse);
     }).catch((error) => {
       self.client.postMessage({error: error.message});
     });
-  } else if (evt.data === 'change-method-2' && self.changePaymentMethod !== null) {
+  } else if (evt.data === 'change-method-2' && self.paymentRequestEvent !== null && self.paymentRequestEvent.changePaymentMethod) {
     response = response2;
-    self.changePaymentMethod(response.methodName, redact(response)).then((paymentMethodChangeResponse) => {
+    self.paymentRequestEvent.changePaymentMethod(response.methodName, redact(response)).then((paymentMethodChangeResponse) => {
       self.client.postMessage(paymentMethodChangeResponse);
     }).catch((error) => {
       self.client.postMessage({error: error.message});
     });
   } else {
-    console.log('Unrecognized message: ' + evt.data);
+    self.client.postMessage({error: 'Service worker did not recognize the message "' + evt.data + '".'});
   }
 });
 
@@ -37,8 +37,8 @@ self.addEventListener('paymentrequest', (evt) => {
     return;
   }
 
-  self.changePaymentMethod = evt.changePaymentMethod;
-  evt.respondWith(new Promise((resolve) => {
+  self.paymentRequestEvent = evt;
+  self.paymentRequestEvent.respondWith(new Promise((resolve) => {
     self.resolver = resolve;
     self.client = evt.openWindow('confirm.html');
   }));
