a08cd9d1bada6d3a3b0b9f64d2009fc808f99320
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2019-04-18 12:38:07 -0400

    Stop using self.client

--- pr/apps/pmc/app.js
+++ pr/apps/pmc/app.js
@@ -7,6 +7,14 @@ self.paymentRequestEvent = null;
 self.resolver = null;
 self.response = response1;
 
+function notifyPaymentMethodChanged(messageDestination) {
+  self.paymentRequestEvent.changePaymentMethod(self.response.methodName, redact(self.response)).then((paymentMethodChangeResponse) => {
+    messageDestination.postMessage(paymentMethodChangeResponse);
+  }).catch((error) => {
+    messageDestination.postMessage({error: error.message});
+  });
+}
+
 self.addEventListener('message', (evt) => {
   if (evt.data === 'confirm') {
     if (self.resolver !== null) {
@@ -16,26 +24,18 @@ self.addEventListener('message', (evt) => {
       evt.source.postMessage({error: 'Service worker cannot confirm payment because there is no resolver function.'});
     }
   } else if (evt.data === 'change-method-1') {
-    response = response1;
+    self.response = response1;
     if (self.paymentRequestEvent !== null && self.paymentRequestEvent.changePaymentMethod) {
-      self.paymentRequestEvent.changePaymentMethod(response.methodName, redact(response)).then((paymentMethodChangeResponse) => {
-        self.client.postMessage(paymentMethodChangeResponse);
-      }).catch((error) => {
-        self.client.postMessage({error: error.message});
-      });
+      notifyPaymentMethodChanged(evt.source);
     } else {
-      evt.source.postMessage({error: 'Service worker cannot change payment method because there is payment request event or no change payment method feature'});
+      evt.source.postMessage({error: 'Service worker cannot change payment method. There is no payment request event or no change payment method feature.'});
     }
   } else if (evt.data === 'change-method-2') {
-    response = response2;
+    self.response = response2;
     if (self.paymentRequestEvent !== null && self.paymentRequestEvent.changePaymentMethod) {
-      self.paymentRequestEvent.changePaymentMethod(response.methodName, redact(response)).then((paymentMethodChangeResponse) => {
-        self.client.postMessage(paymentMethodChangeResponse);
-      }).catch((error) => {
-        self.client.postMessage({error: error.message});
-      });
+      notifyPaymentMethodChanged(evt.source);
     } else {
-      evt.source.postMessage({error: 'Service worker cannot change payment method because there is payment request event or no change payment method feature'});
+      evt.source.postMessage({error: 'Service worker cannot change payment method. There is no payment request event or no change payment method feature.'});
     }
   } else {
     evt.source.postMessage({error: 'Service worker did not recognize the message "' + evt.data + '".'});
