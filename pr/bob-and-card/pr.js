99f56e08e3c7078646d7bef569e92fc85e7ba171
Danyao Wang <danyao@chromium.org>
2019-06-28 13:39:36 -0400

    Add a test case of URL-based payment method and basic-card

---
+++ pr/bob-and-card/pr.js
@@ -0,0 +1,87 @@
+/* exported onBuyClicked */
+
+/**
+ * Initializes the payment request object.
+ * @return {PaymentRequest} The payment request object.
+ */
+function buildPaymentRequest() {
+  if (!window.PaymentRequest) {
+    return null;
+  }
+
+  const supportedInstruments = [{
+    supportedMethods: 'basic-card',
+  }, {
+    supportedMethods: 'https://bobpay.xyz/pay',
+  }];
+
+  const details = {
+    total: {
+      label: 'Tots',
+      amount: {
+        currency: 'USD',
+        value: '1.00',
+      },
+    },
+  };
+
+  let request = null;
+
+  try {
+    request = new PaymentRequest(supportedInstruments, details);
+    if (request.canMakePayment) {
+      request.canMakePayment().then(function(result) {
+        info(result ? 'Can make payment' : 'Cannot make payment');
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+
+    if (request.onpaymentmethodchange !== undefined) {
+      info('Will print out payment method change event details here.');
+      request.addEventListener('paymentmethodchange', (evt) => {
+        info('Payment method change event: ' + JSON.stringify({'methodName': evt.methodName, 'methodDetails': evt.methodDetails}, undefined, 2));
+      });
+    }
+  } catch (e) {
+    error('Developer mistake: \'' + e + '\'');
+  }
+
+  return request;
+}
+
+let request = buildPaymentRequest();
+
+/**
+ * Launches payment request for Android Pay.
+ */
+function onBuyClicked() {
+  if (!window.PaymentRequest || !request) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  try {
+    request.show()
+      .then(function(instrumentResponse) {
+        window.setTimeout(function() {
+          instrumentResponse.complete('success')
+            .then(function() {
+              done('This is a demo website. No payment will be processed.',
+                instrumentResponse);
+            })
+            .catch(function(err) {
+              error(err);
+              request = buildPaymentRequest();
+            });
+        }, 2000);
+      })
+      .catch(function(err) {
+        error(err);
+        request = buildPaymentRequest();
+      });
+  } catch (e) {
+    error('Developer mistake: \'' + e + '\'');
+    request = buildPaymentRequest();
+  }
+}
