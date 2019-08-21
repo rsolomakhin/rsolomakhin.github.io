4e34f1b4a5b8278bf9a958338059fc1fd8e00a76
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2019-08-21 18:03:14 -0400

    Micro

--- pr/apps/micro/app.js
+++ pr/apps/micro/app.js
@@ -1,12 +1,20 @@
 self.addEventListener('canmnakepayment', (evt) => {
-  evt.respondWith(true);
+  if (evt.respondWith2 && evt.currency) { 
+    return evt.respondWith2({
+      canMakePayment: true,
+      readyForMicrotransaction: evt.currency === 'USD',
+      accountBalance: '18.00',
+    });
+  } else {
+    evt.respondWith(true);
+  }
 });
 
 self.addEventListener('paymentrequest', (evt) => {
-    evt.respondWith({
-        methodName: 'https://rsolomakhin.github.io',
-        details: {
-            token: '1234567890',
-        },
-    });
+  evt.respondWith({
+    methodName: 'https://rsolomakhin.github.io',
+    details: {
+      token: '1234567890',
+    },
+  });
 });
