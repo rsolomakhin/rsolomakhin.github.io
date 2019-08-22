1140c4ca9f9fe27d665fd104c67930bca9db48ac
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2019-08-22 16:20:55 -0400

    Clean up

--- pr/apps/password-test/password.js
+++ pr/apps/password-test/password.js
@@ -1,6 +1,11 @@
 async function storePassword() {
   try {
     document.getElementById('msg').innerHTML = '';
+    if (document.getElementById('username').value == '' ||
+        document.getElementById('password').value == '') {
+      document.getElementById('status').innerHTML = 'Please fill in a username and password to store!';
+      return;
+    }
     document.getElementById('status').innerHTML = 'Storing password...';
     const request = new PaymentRequest([{
       supportedMethods: 'https://rsolomakhin.github.io/pr/apps/password',
@@ -18,9 +23,9 @@ async function storePassword() {
         }
       }
     });
+    const response = await request.show();
     document.getElementById('username').value = '';
     document.getElementById('password').value = '';
-    const response = await request.show();
     document.getElementById('msg').innerHTML = JSON.stringify(response.details);
     document.getElementById('status').innerHTML = 'Username and password stored.';
     await response.complete('success');
