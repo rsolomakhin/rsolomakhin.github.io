622152beceacfc770f2f0b9229e7c410c67d0569
Sahel Sharify <sahel@chromium.org>
2019-12-09 16:08:30 -0500

    Demo Payment App for Shipping Option Change.

---
+++ pr/apps/shipping-and-contact-delegation/soc/data.js
@@ -0,0 +1,28 @@
+// The data should be stored on the server.
+
+const responseData = {
+  methodName: 'basic-card',
+  details: {
+      cardNumber: '378282246310005',
+      cardType: 'amex',
+      cardSecurityCode: '2302',
+      cardholderName: 'Jon Doe',
+      expiryMonth: '09',
+      expiryYear: '2029',
+  },
+  shippingAddress: {
+      addressLine: [
+        '340 Main St',
+      ],
+      city: 'Venice',
+      country: 'US',
+      dependentLocality: '',
+      organization: 'Google',
+      phone: '+13103106000',
+      postalCode: '90291',
+      recipient: 'Jon Doe',
+      region: 'CA',
+      sortingCode: ''
+  },
+  shippingOption: '',
+};
