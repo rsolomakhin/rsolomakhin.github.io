b9e7a2237b4291cc040e2fbeb3510a5c6b6c869a
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2020-01-16 16:06:31 -0500

    Add logs

--- pr/apps/anti/app.js
+++ pr/apps/anti/app.js
@@ -1,8 +1,16 @@
+self.addEventListener('install', (evt) => {
+    console.log('Installed');
+});
+
 self.addEventListener('canmnakepayment', (evt) => {
-  evt.respondWith(false);
+    evt.respondWith(new Promise((resolve) => {
+        console.log('Cannot make payments');
+        resolve(false);
+    });
 });
 
 self.addEventListener('paymentrequest', (evt) => {
+    console.log('Payment requested');
     evt.respondWith({
         methodName: 'https://rsolomakhin.github.io',
         details: {
