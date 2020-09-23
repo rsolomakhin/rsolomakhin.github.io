5a614e1f4e78d3d0d753167d5047a6a2843797ff
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2020-09-23 17:24:03 -0400

    Cleanup

--- pr/test/sw_payment.js
+++ pr/test/sw_payment.js
@@ -1,13 +1,4 @@
 self.addEventListener('paymentrequest', e => {
-  payment_request_event = e;
-
   e.respondWith({});
-  e.openWindow(checkoutURL).then(client => {
-    if (client === null) {
-      resolver.reject('Failed to open window');
-    }
-  }).catch(err => {
-    resolver.reject(err);
-  });
+  e.openWindow(checkoutURL).then(client => {}).catch(err => {});
 });
-
