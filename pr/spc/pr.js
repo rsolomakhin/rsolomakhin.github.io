befdf910d75e03a3c7e9776a4c69ee91e2e4d2e3
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2021-06-17 09:58:03 -0400

    More verbose logging of outputs for SPC to the webpage

--- pr/spc/pr.js
+++ pr/spc/pr.js
@@ -1,8 +1,114 @@
 /* exported createPaymentCredential */
 /* exported onBuyClicked */
-
 const textEncoder = new TextEncoder();
-
+/**
+ * Converts an ArrayBuffer into a string.
+ */
+function arrayBufferToString(input) {
+  return String.fromCharCode(...new Uint8Array(input));
+}
+/**
+ * Converts an ArrayBuffer into a base64 encoded string.
+ */
+function arrayBufferToBase64(input) {
+  return btoa(arrayBufferToString(input));
+}
+/**
+ * Converts a PaymentResponse or a PublicKeyCredential into an dictionary.
+ */
+function objectToDictionary(input) {
+  let output = {};
+  if (input.requestId) {
+    output.requestId = input.requestId;
+  }
+  if (input.id) {
+    output.id = input.id;
+  }
+  if (input.rawId && input.rawId.constructor === ArrayBuffer) {
+    output.rawId = arrayBufferToBase64(input.rawId);
+  }
+  if (input.response && (input.response.constructor ===
+      AuthenticatorAttestationResponse || input.response.constructor ===
+      AuthenticatorAssertionResponse)) {
+    output.response = objectToDictionary(input);
+  }
+  if (input.attestationObject && input.attestationObject === ArrayBuffer) {
+    output.attestationObject = arrayBufferToBase64(input.attestationObject);
+  }
+  if (input.authenticatorData && input.authenticatorData === ArrayBuffer) {
+    output.authenticatorData = arrayBufferToBase64(input.authenticatorData);
+  }
+  if (input.clientDataJSON && input.clientDataJSON === ArrayBuffer) {
+    output.clientDataJSON = arrayBufferToString(input.clientDataJSON);
+  }
+  if (input.signature && input.signature === ArrayBuffer) {
+    output.signature = arrayBufferToBase64(input.signature);
+  }
+  if (input.signature && input.signature === String) {
+    output.signature = input.signature;
+  }
+  if (input.userHandle && input.userHandle === ArrayBuffer) {
+    output.userHandle = arrayBufferToBase64(input.userHandle);
+  }
+  if (input.type) {
+    output.type = input.type;
+  }
+  if (input.methodName) {
+    output.methodName = input.methodName;
+  }
+  if (input.details) {
+    output.details = objectToDictionary(input.details);
+  }
+  if (input.appid_extension) {
+    output.appid_extension = input.appid_extension;
+  }
+  if (input.challenge) {
+    output.challenge = input.challenge;
+  }
+  if (input.echo_appid_extension) {
+    output.echo_appid_extension = input.echo_appid_extension;
+  }
+  if (input.echo_prf) {
+    output.echo_prf = input.echo_prf;
+  }
+  if (input.prf_not_evaluated) {
+    output.prf_not_evaluated = input.prf_not_evaluated;
+  }
+  if (input.prf_results) {
+    output.prf_results = objectToDictionary(input.prf_results);
+  }
+  if (input.user_handle) {
+    output.user_handle = input.user_handle;
+  }
+  if (input.authenticator_data) {
+    output.authenticator_data = input.authenticator_data;
+  }
+  if (input.client_data_json) {
+    output.client_data_json = atob(input.client_data_json);
+  }
+  if (input.shippingAddress) {
+    output.shippingAddress = input.shippingAddress;
+  }
+  if (input.shippingOption) {
+    output.shippingOption = input.shippingOption;
+  }
+  if (input.payerName) {
+    output.payerName = input.payerName;
+  }
+  if (input.payerEmail) {
+    output.payerEmail = input.payerEmail;
+  }
+  if (input.payerPhone) {
+    output.payerPhone = input.payerPhone;
+  }
+  return output;
+}
+/**
+ * Converts a PaymentResponse or a PublicKeyCredential into a string.
+ */
+function objectToString(input) {
+  return JSON.stringify(objectToDictionary(input), undefined, 2);
+}
 /**
  * Creates a payment credential.
  */
@@ -17,10 +123,10 @@ async function createPaymentCredential(windowLocalStorageIdentifier) {
   };
   const pubKeyCredParams = [{
     type: 'public-key',
-    alg: -7,  // ECDSA, not supported on Windows.
+    alg: -7, // ECDSA, not supported on Windows.
   }, {
     type: 'public-key',
-    alg: -257,  // RSA, supported on Windows.
+    alg: -257, // RSA, supported on Windows.
   }];
   const authenticatorSelection = {
     userVerification: 'required',
@@ -33,19 +139,17 @@ async function createPaymentCredential(windowLocalStorageIdentifier) {
     authenticatorSelection,
   };
   try {
-    const publicKeyCredential = await navigator.credentials.create({payment});
-    window.localStorage.setItem(
-        windowLocalStorageIdentifier,
-        btoa(String.fromCharCode(...new Uint8Array(
-            publicKeyCredential.rawId))));
-    info(windowLocalStorageIdentifier + ': Credential ' +
-         window.localStorage.getItem(windowLocalStorageIdentifier) +
-         ' enrolled.');
+    const publicKeyCredential = await navigator.credentials.create({
+      payment
+    });
+    window.localStorage.setItem(windowLocalStorageIdentifier,
+      arrayBufferToBase64(publicKeyCredential.rawId));
+    info(windowLocalStorageIdentifier + ': Credential ' + window.localStorage
+      .getItem(windowLocalStorageIdentifier) + ' enrolled.');
   } catch (err) {
     error(err);
   }
 }
-
 /**
  * Initializes the payment request object.
  * @return {PaymentRequest} The payment request object.
@@ -54,9 +158,7 @@ async function buildPaymentRequest(windowLocalStorageIdentifier) {
   if (!window.PaymentRequest) {
     return null;
   }
-
   let request = null;
-
   try {
     // Documentation:
     // https://github.com/rsolomakhin/secure-payment-confirmation
@@ -64,15 +166,13 @@ async function buildPaymentRequest(windowLocalStorageIdentifier) {
       supportedMethods: 'secure-payment-confirmation',
       data: {
         action: 'authenticate',
-        credentialIds: [Uint8Array.from(
-            atob(window.localStorage.getItem(windowLocalStorageIdentifier)),
-            c => c.charCodeAt(0))],
+        credentialIds: [Uint8Array.from(atob(window.localStorage.getItem(
+          windowLocalStorageIdentifier)), c => c.charCodeAt(0))],
         networkData: textEncoder.encode('network_data'),
         timeout: 60000,
         fallbackUrl: 'https://rsolomakhin.github.io/pr/spc/fallback'
       },
     }];
-
     const details = {
       total: {
         label: 'Total',
@@ -82,15 +182,12 @@ async function buildPaymentRequest(windowLocalStorageIdentifier) {
         },
       },
     };
-
     request = new PaymentRequest(supportedInstruments, details);
   } catch (err) {
     error(err);
   }
-
   return request;
 }
-
 /**
  * Launches payment request for Android Pay.
  */
@@ -99,20 +196,18 @@ async function onBuyClicked(windowLocalStorageIdentifier) {
     error('PaymentRequest API is not supported.');
     return;
   }
-
   const request = await buildPaymentRequest(windowLocalStorageIdentifier);
-  if (!request)
-    return;
-
+  if (!request) return;
   try {
     const instrumentResponse = await request.show();
     await instrumentResponse.complete('success')
-    info(windowLocalStorageIdentifier + ': ' + JSON.stringify(instrumentResponse, undefined, 2));
+    console.log(instrumentResponse);
+    info(windowLocalStorageIdentifier + ': ' + objectToString(
+      instrumentResponse));
   } catch (err) {
     error(err);
   }
 }
-
 async function checkCanMakePayment(windowLocalStorageIdentifier) {
   if (!window.PaymentRequest) {
     error('PaymentRequest API is not supported.');
@@ -120,32 +215,32 @@ async function checkCanMakePayment(windowLocalStorageIdentifier) {
   }
   try {
     const request = await buildPaymentRequest(windowLocalStorageIdentifier);
-    if (!request)
-      return;
+    if (!request) return;
     const result = await request.canMakePayment();
-    info(windowLocalStorageIdentifier + ': ' + (result ? 'Can make payment.' : 'Cannot make payment'));
+    info(windowLocalStorageIdentifier + ': ' + (result ? 'Can make payment.' :
+      'Cannot make payment'));
   } catch (err) {
     error(err);
   }
 }
-
 async function webAuthnGet(windowLocalStorageIdentifier) {
   try {
     const publicKey = {
       challenge: textEncoder.encode('Authentication challenge'),
       userVerification: 'required',
-      allowCredentials: [
-        {
-          transports: ['internal'],
-          type: 'public-key',
-          id: Uint8Array.from(atob(window.localStorage.getItem(windowLocalStorageIdentifier)), c => c.charCodeAt(0)),
-        },
-      ],
+      allowCredentials: [{
+        transports: ['internal'],
+        type: 'public-key',
+        id: Uint8Array.from(atob(window.localStorage.getItem(
+          windowLocalStorageIdentifier)), c => c.charCodeAt(0)),
+      }, ],
     };
-
-    const credentialInfoAssertion = await navigator.credentials.get({publicKey});
+    const credentialInfoAssertion = await navigator.credentials.get({
+      publicKey
+    });
     console.log(credentialInfoAssertion);
-    info('Successful login with credential ' + credentialInfoAssertion.id);
+    info('Successful login with credential: ' + objectToString(
+      credentialInfoAssertion));
   } catch (err) {
     error(err);
   }
