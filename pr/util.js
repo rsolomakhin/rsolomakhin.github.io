1f3223f76037b0436a445fff5452961b7de5907e
Liquan (Max) Gu <maxlg@chromium.org>
2022-02-10 12:02:30 -0500

    update reporting api

--- pr/util.js
+++ pr/util.js
@@ -116,7 +116,7 @@ let options = {
 
 let observer = new ReportingObserver(function(reports, observer) {
   for (const report of reports) {
-    error(report);
+    error(report.type + ": "+ report.body);
   }
 }, options);
 
