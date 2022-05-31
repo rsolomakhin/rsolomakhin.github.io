711bce9959f3031e9f27fadec2f9aff6b865a150
Stephen McGruer <smcgruer@chromium.org>
2022-05-31 13:39:54 -0400

    Change store name for opt-out flow

--- pr/spc-opt-out/pr.js
+++ pr/spc-opt-out/pr.js
@@ -231,7 +231,7 @@ async function buildPaymentRequest(windowLocalStorageIdentifier, showOptOut) {
         networkData: challenge,
         challenge,
         timeout: 60000,
-        payeeOrigin: window.location.origin,
+        payeeOrigin: 'store.example',
         rpId: window.location.hostname,
         showOptOut: !!showOptOut,
       },
