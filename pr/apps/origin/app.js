fcf3974a10469890fd282115ae17d9f7b20c2c19
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2022-09-27 18:27:13 +0000

    Access deprecated identity fields in canmakepayment event

--- pr/apps/origin/app.js
+++ pr/apps/origin/app.js
@@ -1,4 +1,29 @@
 self.addEventListener('canmakepayment', (evt) => {
+  if (evt.paymentRequestOrigin) {
+    console.log(evt.paymentRequestOrigin);
+  } else {
+    console.log("No paymentRequestOrigin field in canmakepayment event.");
+  }
+
+  if (evt.topOrigin) {
+    console.log(evt.topOrigin);
+  } else {
+    console.log("No topOrigin field in canmakepayment event.");
+  }
+
+  if (evt.methodData) {
+    console.log(evt.methodData);
+  } else {
+    console.log("No methodData field in canmakepayment event.");
+  }
+
+  if (evt.modifiers) {
+    console.log(evt.modifiers);
+  } else {
+    console.log("No modifiers field in canmakepayment event.");
+  }
+
+
   evt.respondWith(true);
 });
 
