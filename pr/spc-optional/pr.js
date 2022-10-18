c12eaa871f3440b5093305f7eaae1387b399a60b
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2022-10-18 19:51:58 +0000

    Export utils to util.js. Implement autofill-field support.

--- pr/spc-optional/pr.js
+++ pr/spc-optional/pr.js
@@ -1,153 +1,6 @@
 /* exported createPaymentCredential */
-/* exported onBuyClicked */
-const textEncoder = new TextEncoder();
-/**
- * Converts an ArrayBuffer into a string.
- */
-function arrayBufferToString(input) {
-  return String.fromCharCode(...new Uint8Array(input));
-}
-/**
- * Converts an ArrayBuffer into a base64 encoded string.
- */
-function arrayBufferToBase64(input) {
-  return btoa(arrayBufferToString(input));
-}
-/**
- * Converts a base64 encoded string into Unit8Array.
- */
-function base64ToArray(input) {
-  return Uint8Array.from(atob(input), c => c.charCodeAt(0))
-}
-/**
- * Converts a PaymentResponse or a PublicKeyCredential into an dictionary.
- */
-function objectToDictionary(input) {
-  let output = {};
-  if (input.requestId) {
-    output.requestId = input.requestId;
-  }
-  if (input.id) {
-    output.id = input.id;
-  }
-  if (input.rawId && input.rawId.constructor === ArrayBuffer) {
-    output.rawId = arrayBufferToBase64(input.rawId);
-  }
-  if (input.response && (input.response.constructor ===
-      AuthenticatorAttestationResponse || input.response.constructor ===
-      AuthenticatorAssertionResponse || input.response.constructor === Object
-      )) {
-    output.response = objectToDictionary(input.response);
-  }
-  if (input.attestationObject && input.attestationObject.constructor ===
-    ArrayBuffer) {
-    output.attestationObject = arrayBufferToBase64(input.attestationObject);
-  }
-  if (input.authenticatorData && input.authenticatorData.constructor ===
-    ArrayBuffer) {
-    output.authenticatorData = arrayBufferToBase64(input.authenticatorData);
-  }
-  if (input.authenticatorData && input.authenticatorData.constructor ===
-    String) {
-    output.authenticatorData = input.authenticatorData;
-  }
-  if (input.clientDataJSON && input.clientDataJSON.constructor ===
-    ArrayBuffer) {
-    output.clientDataJSON = arrayBufferToString(input.clientDataJSON);
-  }
-  if (input.clientDataJSON && input.clientDataJSON.constructor ===
-    String) {
-    output.clientDataJSON = atob(input.clientDataJSON);
-  }
-  if (input.info) {
-    output.info = objectToDictionary(input.info);
-  }
-  if (input.signature && input.signature.constructor === ArrayBuffer) {
-    output.signature = arrayBufferToBase64(input.signature);
-  }
-  if (input.signature && input.signature.constructor === String) {
-    output.signature = input.signature;
-  }
-  if (input.userHandle && input.userHandle.constructor === ArrayBuffer) {
-    output.userHandle = arrayBufferToBase64(input.userHandle);
-  }
-  if (input.userHandle && input.userHandle.constructor === String) {
-    output.userHandle = input.userHandle;
-  }
-  if (input.type) {
-    output.type = input.type;
-  }
-  if (input.methodName) {
-    output.methodName = input.methodName;
-  }
-  if (input.details) {
-    output.details = objectToDictionary(input.details);
-  }
-  if (input.appid_extension) {
-    output.appid_extension = input.appid_extension;
-  }
-  if (input.challenge) {
-    output.challenge = input.challenge;
-  }
-  if (input.echo_appid_extension) {
-    output.echo_appid_extension = input.echo_appid_extension;
-  }
-  if (input.echo_prf) {
-    output.echo_prf = input.echo_prf;
-  }
-  if (input.prf_not_evaluated) {
-    output.prf_not_evaluated = input.prf_not_evaluated;
-  }
-  if (input.prf_results) {
-    output.prf_results = objectToDictionary(input.prf_results);
-  }
-  if (input.user_handle) {
-    output.user_handle = input.user_handle;
-  }
-  if (input.authenticator_data) {
-    output.authenticator_data = input.authenticator_data;
-  }
-  if (input.client_data_json) {
-    output.client_data_json = atob(input.client_data_json);
-  }
-  if (input.shippingAddress) {
-    output.shippingAddress = input.shippingAddress;
-  }
-  if (input.shippingOption) {
-    output.shippingOption = input.shippingOption;
-  }
-  if (input.payerName) {
-    output.payerName = input.payerName;
-  }
-  if (input.payerEmail) {
-    output.payerEmail = input.payerEmail;
-  }
-  if (input.payerPhone) {
-    output.payerPhone = input.payerPhone;
-  }
-  return output;
-}
-/**
- * Converts a PaymentResponse or a PublicKeyCredential into a string.
- */
-function objectToString(input) {
-  return JSON.stringify(objectToDictionary(input), undefined, 2);
-}
-
-// Return whether or not SPC supports residentKey 'preferred' (instead of just
-// 'required'). There is unfortunately no way to feature detect this, so we
-// have to do a version check.
-function spcSupportsPreferred() {
-  // This will be true for not just Chrome but also Edge/etc, but that's fine.
-  const match = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
-  if (!match)
-    return false;
-
-  const version = parseInt(match[2], 10);
-  // https://crrev.com/130fada41 landed in 106.0.5228.0, but we assume that any
-  // 106 will do for simplicity.
-  return version >= 106;
-}
+/* exported pretendSubmitForm */
+/* exported optionallyGetSpcFrom */
 
 async function createCredentialCompat() {
   const rp = {
@@ -182,9 +35,9 @@ async function createCredentialCompat() {
     const publicKey = {
       rp,
       user: {
-        name: 'user@domain',
+        name: 'Troy ····',
         id: Uint8Array.from(String(Math.random()*999999999), c => c.charCodeAt(0)),
-        displayName: 'User',
+        displayName: '',
       },
       challenge,
       pubKeyCredParams,
@@ -208,46 +61,49 @@ async function createCredentialCompat() {
 /**
  * Creates a payment credential.
  */
-async function createPaymentCredential(windowLocalStorageIdentifier) {
+async function createPaymentCredential(windowLocalStorageItemKey) {
   try {
     const publicKeyCredential = await createCredentialCompat();
     console.log(publicKeyCredential);
-    window.localStorage.setItem(windowLocalStorageIdentifier,
+    window.localStorage.setItem(windowLocalStorageItemKey,
       arrayBufferToBase64(publicKeyCredential.rawId));
-    info(windowLocalStorageIdentifier + ' enrolled: ' + objectToString(
+    info(windowLocalStorageItemKey + ' enrolled: ' + objectToString(
       publicKeyCredential));
   } catch (err) {
     error(err);
   }
 }
+
 /**
  * Initializes the payment request object.
+ * @param {string} autofillField - The name of the autofill field
+ * @param {string} windowLocalStorageItemKey - The key in window local
+ * storage for looking up the previously saved credential identifier.
  * @return {PaymentRequest} The payment request object.
  */
-async function buildPaymentRequest(windowLocalStorageIdentifier) {
+async function buildPaymentRequest(autofillField, windowLocalStorageItemKey) {
   if (!window.PaymentRequest) {
     return null;
   }
   let request = null;
   try {
-    // Documentation:
-    // https://github.com/w3c/secure-payment-confirmation
     const challenge = textEncoder.encode('network_data');
     const updatedInstrument = {
-      displayName: 'My Troy Card',
-      icon: 'https://rsolomakhin.github.io/pr/spc/troy-alt-logo.png',
+      displayName: 'Troy ····',
+      icon: 'https://rsolomakhin.github.io/pr/spc/troy.png',
     };
     const supportedInstruments = [{
       supportedMethods: 'secure-payment-confirmation',
       data: {
         credentialIds: [base64ToArray(window.localStorage.getItem(
-          windowLocalStorageIdentifier))],
+          windowLocalStorageItemKey))],
         instrument: updatedInstrument,
         networkData: challenge,
         challenge,
         timeout: 60000,
         payeeOrigin: window.location.origin,
         rpId: window.location.hostname,
+        autofillField
       },
     }];
     const details = {
@@ -265,59 +121,32 @@ async function buildPaymentRequest(windowLocalStorageIdentifier) {
   }
   return request;
 }
-/**
- * Launches payment request for Android Pay.
- */
-async function onBuyClicked(windowLocalStorageIdentifier) {
+
+async function optionallyGetSpcFrom(autofillField, windowLocalStorageItemKey) {
   if (!window.PaymentRequest) {
     error('PaymentRequest API is not supported.');
     return;
   }
-  const request = await buildPaymentRequest(windowLocalStorageIdentifier);
+  if (!window.PaymentRequest.AutofillField) {
+    error('Showing SPC in Autofill popup is not supported.');
+    return;
+  }
+  const request = await buildPaymentRequest(autofillField, windowLocalStorageItemKey);
   if (!request) return;
   try {
     const instrumentResponse = await request.show();
-    await instrumentResponse.complete('success')
-    console.log(instrumentResponse);
-    info(windowLocalStorageIdentifier + ' payment response: ' +
+    await instrumentResponse.complete('success');
+    info(windowLocalStorageItemKey + ' payment response: ' +
       objectToString(instrumentResponse));
   } catch (err) {
     error(err);
   }
 }
-async function checkCanMakePayment(windowLocalStorageIdentifier) {
-  if (!window.PaymentRequest) {
-    error('PaymentRequest API is not supported.');
-    return;
-  }
-  try {
-    const request = await buildPaymentRequest(windowLocalStorageIdentifier);
-    if (!request) return;
-    const result = await request.canMakePayment();
-    info((result ? 'Can make payment.' : 'Cannot make payment'));
-  } catch (err) {
-    error(err);
-  }
-}
-async function webAuthnGet(windowLocalStorageIdentifier) {
-  try {
-    const publicKey = {
-      challenge: textEncoder.encode('Authentication challenge'),
-      userVerification: 'required',
-      allowCredentials: [{
-        transports: ['internal'],
-        type: 'public-key',
-        id: base64ToArray(window.localStorage.getItem(
-          windowLocalStorageIdentifier)),
-      }, ],
-    };
-    const credentialInfoAssertion = await navigator.credentials.get({
-      publicKey
-    });
-    console.log(credentialInfoAssertion);
-    info('Successful login with ' + windowLocalStorageIdentifier + ': ' +
-      objectToString(credentialInfoAssertion));
-  } catch (err) {
-    error(err);
+
+function pretendSubmitForm() {
+  if (document.getElementById('phone-field').nodeValue) {
+    info('Pretend SMS sent to the provided phone number.');
+  } else {
+    error('No phone number provided.');
   }
 }
