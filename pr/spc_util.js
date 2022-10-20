017119cc48ed985f2966dc49cd2b69f1d9a95d6d
Stephen McGruer <smcgruer@chromium.org>
2022-10-20 18:02:06 -0400

    [SPC] Move rest of utility output functions to spc_util.js

--- pr/spc_util.js
+++ pr/spc_util.js
@@ -124,8 +124,80 @@ function createSPCPaymentRequest(spcData) {
 }
 
 /**
+ * Converts a PaymentResponse or a PublicKeyCredential into a string.
+ */
+function objectToString(input) {
+  return JSON.stringify(objectToDictionary(input), undefined, 2);
+}
+
+/**
+ * Converts a PaymentResponse or a PublicKeyCredential into an dictionary.
+ */
+function objectToDictionary(input) {
+  let output = {};
+  if (input.requestId) {
+    output.requestId = input.requestId;
+  }
+  if (input.id) {
+    output.id = input.id;
+  }
+  if (input.rawId && input.rawId.constructor === ArrayBuffer) {
+    output.rawId = arrayBufferToBase64(input.rawId);
+  }
+  if (input.response && (input.response.constructor ===
+      AuthenticatorAttestationResponse || input.response.constructor ===
+      AuthenticatorAssertionResponse || input.response.constructor === Object
+      )) {
+    output.response = objectToDictionary(input.response);
+  }
+  if (input.attestationObject && input.attestationObject.constructor ===
+    ArrayBuffer) {
+    output.attestationObject = arrayBufferToBase64(input.attestationObject);
+  }
+  if (input.authenticatorData && input.authenticatorData.constructor ===
+    ArrayBuffer) {
+    output.authenticatorData = arrayBufferToBase64(input.authenticatorData);
+  }
+  if (input.authenticatorData && input.authenticatorData.constructor ===
+    String) {
+    output.authenticatorData = input.authenticatorData;
+  }
+  if (input.clientDataJSON && input.clientDataJSON.constructor ===
+    ArrayBuffer) {
+    output.clientDataJSON = arrayBufferToString(input.clientDataJSON);
+  }
+  if (input.clientDataJSON && input.clientDataJSON.constructor ===
+    String) {
+    output.clientDataJSON = atob(input.clientDataJSON);
+  }
+  if (input.info) {
+    output.info = objectToDictionary(input.info);
+  }
+  if (input.signature && input.signature.constructor === ArrayBuffer) {
+    output.signature = arrayBufferToBase64(input.signature);
+  }
+  if (input.signature && input.signature.constructor === String) {
+    output.signature = input.signature;
+  }
+  if (input.userHandle && input.userHandle.constructor === ArrayBuffer) {
+
+/**
  * Converts a base64 encoded string into Unit8Array.
  */
 function base64ToArray(input) {
   return Uint8Array.from(atob(input), c => c.charCodeAt(0))
+}
+
+/**
+ * Converts an ArrayBuffer into a base64 encoded string.
+ */
+function arrayBufferToBase64(input) {
+  return btoa(arrayBufferToString(input));
+}
+
+/**
+ * Converts an ArrayBuffer into a string.
+ */
+function arrayBufferToString(input) {
+  return String.fromCharCode(...new Uint8Array(input));
 }
