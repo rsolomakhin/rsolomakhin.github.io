721ca918d638b5e38e75d3186b5000e1281889a8
Stephen McGruer <smcgruer@chromium.org>
2022-10-24 09:20:38 -0400

    [SPC] Add hidden metrics testing page
    For investigating what JourneyLogger logs for SPC flows

---
+++ pr/spc-metrics-test/pr.js
@@ -0,0 +1,73 @@
+/**
+ * Creates a payment credential.
+ */
+async function createPaymentCredential(windowLocalStorageIdentifier) {
+  try {
+    const publicKeyCredential = await createCredential(/*setPaymentExtension=*/true);
+    console.log(publicKeyCredential);
+    window.localStorage.setItem(windowLocalStorageIdentifier,
+      arrayBufferToBase64(publicKeyCredential.rawId));
+    info(windowLocalStorageIdentifier + ' enrolled: ' + objectToString(
+      publicKeyCredential));
+  } catch (err) {
+    error(err);
+  }
+}
+
+let gPaymentRequest = null;
+
+// TODO: Show opt out option
+async function createPaymentRequest(windowLocalStorageIdentifier) {
+  try {
+    gPaymentRequest = await createSPCPaymentRequest({
+      credentialIds: [base64ToArray(window.localStorage.getItem(windowLocalStorageIdentifier))],
+    });
+    info('Created PaymentRequest object');
+  } catch (e) {
+    error(e);
+  }
+}
+
+async function callCanMakePayment() {
+  if (!gPaymentRequest) {
+    error('gPaymentRequest is not set');
+    return;
+  }
+
+  try {
+    const cmp = gPaymentRequest.canMakePayment();
+    info('Called canMakePayment, result: ' + cmp);
+  } catch (e) {
+    error(e);
+  }
+}
+
+async function callHasEnrolledInstrument() {
+  if (!gPaymentRequest) {
+    error('gPaymentRequest is not set');
+    return;
+  }
+
+  try {
+    const hei = gPaymentRequest.hasEnrolledInstrument();
+    info('Called hasEnrolledInstrument, result: ' + hei);
+  } catch (e) {
+    error(e);
+  }
+}
+
+async function callShow() {
+  if (!gPaymentRequest) {
+    error('gPaymentRequest is not set');
+    return;
+  }
+
+  try {
+    const instrumentResponse = await gPaymentRequest.show();
+    await instrumentResponse.complete('success')
+    console.log(instrumentResponse);
+    info('Called show, result: ' + objectToString(instrumentResponse));
+  } catch (err) {
+    error(err);
+  }
+}
