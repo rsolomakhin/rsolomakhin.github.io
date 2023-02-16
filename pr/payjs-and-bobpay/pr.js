1f75de386a049012e139d1b92b0e6af4e0af3d9b
Stephen McGruer <smcgruer@chromium.org>
2023-02-16 17:57:07 -0500

    Add payjs and bobpay hidden demo

---
+++ pr/payjs-and-bobpay/pr.js
@@ -0,0 +1,145 @@
+let baseRequest;
+let baseCardPaymentMethod;
+let paymentsClient;
+try {
+  baseRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+  };
+  const allowedCardNetworks = ['AMEX', 'DISCOVER', 'INTERAC', 'JCB', 'MASTERCARD', 'VISA'];
+  const allowedCardAuthMethods = ['PAN_ONLY', 'CRYPTOGRAM_3DS'];
+  baseCardPaymentMethod = {
+    type: 'CARD',
+    parameters: {
+      allowedAuthMethods: allowedCardAuthMethods,
+      allowedCardNetworks: allowedCardNetworks,
+    },
+  };
+} catch (err) {
+  error(JSON.stringify(err, undefined, 2));
+}
+
+async function payButtonClickHandler() {
+  try {
+    const tokenizationSpecification = {
+      type: 'PAYMENT_GATEWAY',
+      parameters: {
+      gateway: 'stripe',
+      'stripe:version': '2018-10-31',
+      // Please use your own Stripe live public key.
+      'stripe:publishableKey': 'pk_live_lNk21zqKM2BENZENh3rzCUgo',
+      }
+    };
+    const cardPaymentMethod = Object.assign(
+      {tokenizationSpecification: tokenizationSpecification},
+      baseCardPaymentMethod,
+    );
+    const paymentDataRequest = Object.assign({}, baseRequest);
+    paymentDataRequest.allowedPaymentMethods = [cardPaymentMethod];
+    paymentDataRequest.transactionInfo = {
+      totalPriceStatus: 'FINAL',
+      totalPrice: '1.00',
+      currencyCode: 'USD',
+      countryCode: 'US',
+    };
+    // Please use your own Google Pay merchant ID.
+    paymentDataRequest.merchantInfo = {
+      merchantName: 'Rouslan Solomakhin',
+      merchantId: '00184145120947117657',
+    };
+    const paymentData = await paymentsClient.loadPaymentData(paymentDataRequest);
+    info(JSON.stringify(paymentData, undefined, 2));
+  } catch (err) {
+    error(JSON.stringify(err, undefined, 2));
+  }
+}
+
+async function addPayButton() {
+  try {
+    paymentsClient = new google.payments.api.PaymentsClient({environment:'PRODUCTION'});
+    const isReadyToPayRequest = Object.assign({}, baseRequest);
+    isReadyToPayRequest.allowedPaymentMethods = [baseCardPaymentMethod];
+    const isReadyToPayResponse = await paymentsClient.isReadyToPay(isReadyToPayRequest);
+    if (!isReadyToPayResponse.result) {
+      error("Not ready to pay");
+      return;
+    }
+    const button = paymentsClient.createButton({onClick: payButtonClickHandler});
+    document.getElementById('buttonContainer').appendChild(button);
+  } catch (err) {
+    error(JSON.stringify(err, undefined, 2));
+  }
+}
+
+bobPayButton = document.getElementById('bobPayButton').addEventListener('click', () => {
+  const supportedInstruments = [{
+    supportedMethods: 'https://www.stephenmcgruer.com/BobBucksAndroidPaymentApp/payment-manifest.json'
+  }];
+
+  const details = {
+    total: {
+      label: 'Payment',
+      amount: {
+        currency: 'USD',
+        value: '1.00',
+      },
+    },
+    displayItems: [{
+      label: 'Line item 1',
+      amount: {
+        currency: 'USD',
+        value: '2.00',
+      },
+    }, {
+      label: 'Discount',
+      amount: {
+        currency: 'USD',
+        value: '-1.00',
+      },
+    }],
+  };
+
+  let request = null;
+
+  try {
+    request = new PaymentRequest(supportedInstruments, details);
+    if (request.canMakePayment) {
+      request.canMakePayment().then(function(result) {
+        info(result ? 'Can make payment' : 'Cannot make payment');
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+    if (request.hasEnrolledInstrument) {
+      request.hasEnrolledInstrument().then(function(result) {
+        info(result ? 'Has enrolled instrument' : 'No enrolled instrument');
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+  }
+
+  try {
+    request.show()
+      .then(handlePaymentResponse)
+      .catch(function(err) {
+        error(err);
+      });
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+  }
+});
+
+function handlePaymentResponse(response) {
+    response.complete('success')
+      .then(function() {
+        done('This is a demo website. No payment will be processed.', response);
+      })
+      .catch(function(err) {
+        error(err);
+        request = buildPaymentRequest();
+      });
+}
+
