f96d6f22caf341c29e38c16bbfd275f6e3b95410
Stephen McGruer <smcgruer@chromium.org>
2023-02-23 08:17:08 -0500

    Move icon-size test apps under a single directory

--- pr/ph-icon-size/pr.js
+++ pr/ph-icon-size/pr.js
@@ -8,7 +8,7 @@ function buildPaymentRequest(size) {
   }
 
   const supportedInstruments = [{
-    supportedMethods: `https://rsolomakhin.github.io/pr/apps/icon-${size}-test/payment_method_manifest.json`
+    supportedMethods: `https://rsolomakhin.github.io/pr/apps/icon-sizes/icon-${size}-test/payment_method_manifest.json`
   }];
 
   const details = {
