9f86880a23322df7289f085eefe1aa3c447bd99f
Stephen McGruer <smcgruer@chromium.org>
2023-03-14 08:12:31 -0400

    Fixup

--- pr/apps/with-errors/never-calls-openWindow/app.js
+++ pr/apps/with-errors/never-calls-openWindow/app.js
@@ -9,6 +9,12 @@ self.addEventListener('paymentrequest', (evt) => {
       console.log('paymentrequest event detected, waiting 10s');
       setTimeout(() => {
         console.log('Resolving promise without calling openWindow');
+	const response = {
+	  // The methodName is purely informative to the caller.
+          methodName: 'https://rsolomakhin.github.io/pr/apps/with-errors/never-calls-openWindow/',
+          details: {},
+        };
+        resolve(response);
       }, 10 * 1000);
     }));
 });
