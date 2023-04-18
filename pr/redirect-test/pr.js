744104c0e9257d3ced0f42f22d2e3a6b2e1aeafd
Stephen McGruer <smcgruer@chromium.org>
2023-04-18 16:06:32 -0400

    [Tmp] Redirect test site

---
+++ pr/redirect-test/pr.js
@@ -0,0 +1,98 @@
+/**
+ * Initializes the payment request object.
+ * @return {PaymentRequest} The payment request object.
+ */
+function buildPaymentRequest() {
+  if (!window.PaymentRequest) {
+    return null;
+  }
+
+  const supportedInstruments = [{
+    supportedMethods: 'https://payment-request-redirect.glitch.me/redirect',
+  }];
+
+  const details = {
+    total: {
+      label: 'Donation',
+      amount: {
+        currency: 'USD',
+        value: '55.00',
+      },
+    },
+    displayItems: [{
+      label: 'Original donation amount',
+      amount: {
+        currency: 'USD',
+        value: '65.00',
+      },
+    }, {
+      label: 'Friends and family discount',
+      amount: {
+        currency: 'USD',
+        value: '-10.00',
+      },
+    }],
+  };
+
+  let request = null;
+
+  try {
+    request = new PaymentRequest(supportedInstruments, details);
+    if (request.canMakePayment) {
+      request.canMakePayment().then(function(result) {
+        info(result ? 'Can make payment' : 'Cannot make payment');
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+    if (request.hasEnrolledInstrument) {
+      request.hasEnrolledInstrument().then(function(result) {
+        info(result ? 'Has enrolled instrument' : 'No enrolled instrument');
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+  }
+
+  return request;
+}
+
+let request = buildPaymentRequest();
+
+/**
+ * Handles the response from PaymentRequest.show().
+ */
+function handlePaymentResponse(response) {
+    response.complete('success')
+      .then(function() {
+        done('This is a demo website. No payment will be processed.', response);
+      })
+      .catch(function(err) {
+        error(err);
+        request = buildPaymentRequest();
+      });
+}
+
+/**
+ * Launches payment request.
+ */
+function onBuyClicked() { // eslint-disable-line no-unused-vars
+  if (!window.PaymentRequest || !request) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  try {
+    request.show()
+      .then(handlePaymentResponse)
+      .catch(function(err) {
+        error(err);
+        request = buildPaymentRequest();
+      });
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+    request = buildPaymentRequest();
+  }
+}
