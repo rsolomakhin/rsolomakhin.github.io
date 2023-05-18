66446fe2f1c44eda0192547b12b3ee863d9eba32
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2023-05-18 15:58:45 +0000

    Handle errors

--- pr/apps/self-signed/installer.js
+++ pr/apps/self-signed/installer.js
@@ -1,6 +1,6 @@
 async function runTest() {
   try {
-    new PaymentRequest([{supportedMethods: window.location.href + 'manifest.json'}], {total: {label: 'Total', amount: {value: '0.01', currency: 'USD'}}}).show();
+    await new PaymentRequest([{supportedMethods: window.location.href + 'manifest.json'}], {total: {label: 'Total', amount: {value: '0.01', currency: 'USD'}}}).show();
   } catch (e) {
     error(e);
   }
