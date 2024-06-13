d1d23a36cef5ac92560aa841db61949fc3a5ba07
Stephen McGruer <smcgruer@chromium.org>
2024-06-13 13:23:13 -0400

    Add applepay-beta

---
+++ pr/applepay-beta/pr.js
@@ -0,0 +1,113 @@
+/**
+ * Initializes the payment request object.
+ * @return {PaymentRequest} The payment request object.
+ */
+function buildPaymentRequest() {
+  if (!window.PaymentRequest) {
+    return null;
+  }
+
+  const supportedInstruments = [{
+    "supportedMethods": "https://apple.com/apple-pay",
+    "data": {
+        "version": 3,
+        // TODO: Should we register for our own merchantIdentifier? This is
+        // taken from https://applepaydemo.apple.com/payment-request-api
+        "merchantIdentifier": "merchant.com.apdemo",
+        "merchantCapabilities": [
+            "supports3DS"
+        ],
+        "supportedNetworks": [
+            "amex",
+            "discover",
+            "masterCard",
+            "visa"
+        ],
+        "countryCode": "US"
+    }
+  }];
+
+  const details = {
+    total: {
+      label: 'Donation',
+      amount: {
+        currency: 'USD',
+        value: '55.00',
+      },
+    },
+    displayItems: [{
+      label: 'Original donation amount',
+      amount: {
+        currency: 'USD',
+        value: '65.00',
+      },
+    }, {
+      label: 'Friends and family discount',
+      amount: {
+        currency: 'USD',
+        value: '-10.00',
+      },
+    }],
+  };
+
+  let request = null;
+
+  try {
+    request = new PaymentRequest(supportedInstruments, details);
+    if (request.canMakePayment) {
+      request.canMakePayment().then(function(result) {
+        info(result ? 'Can make payment' : 'Cannot make payment');
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+    if (request.hasEnrolledInstrument) {
+      request.hasEnrolledInstrument().then(function(result) {
+        info(result ? 'Has enrolled instrument' : 'No enrolled instrument');
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+  }
+
+  return request;
+}
+
+let request = buildPaymentRequest();
+
+/**
+ * Handles the response from PaymentRequest.show().
+ */
+function handlePaymentResponse(response) {
+    response.complete('success')
+      .then(function() {
+        done('This is a demo website. No payment will be processed.', response);
+      })
+      .catch(function(err) {
+        error(err);
+        request = buildPaymentRequest();
+      });
+}
+
+/**
+ * Launches payment request for Bob Pay.
+ */
+async function onBuyClicked() { // eslint-disable-line no-unused-vars
+  if (!window.PaymentRequest || !request) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  // Temporary, for testing background tab behavior.
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  try {
+    const response = await request.show();
+    await handlePaymentResponse(response);
+  } catch (e) {
+    error('Error: \'' + e.message + '\'');
+    request = buildPaymentRequest();
+  }
+}
