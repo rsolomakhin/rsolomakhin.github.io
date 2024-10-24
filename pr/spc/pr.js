74cdbceb28aef438fb382cef9f8a4dbafb3803b4
Slobodan Pejic <slobodan@google.com>
2024-10-24 11:25:10 -0400

    Update the SPC demo by showing browser bound keys in the extensions.

--- pr/spc/pr.js
+++ pr/spc/pr.js
@@ -11,7 +11,8 @@ async function createPaymentCredential(windowLocalStorageIdentifier) {
     window.localStorage.setItem(windowLocalStorageIdentifier,
       arrayBufferToBase64(publicKeyCredential.rawId));
     info(windowLocalStorageIdentifier + ' enrolled: ' + objectToString(
-      publicKeyCredential));
+      publicKeyCredential) + '\n' + 'Extensions: ' +
+      extensionsOutputToString(publicKeyCredential));
   } catch (err) {
     error(err);
   }
@@ -23,7 +24,24 @@ async function createPaymentCredential(windowLocalStorageIdentifier) {
 async function onBuyClicked(windowLocalStorageIdentifier) {
   try {
     const request = await createSPCPaymentRequest({
-      credentialIds: [base64ToArray(window.localStorage.getItem(windowLocalStorageIdentifier))],
+      credentialIds: [base64ToArray(
+          window.localStorage.getItem(windowLocalStorageIdentifier))],
+      // `browserBoundPubKeyCredParams` does not need to be set and will default
+      // to the same values listed here.
+      browserBoundPubKeyCredParams: [
+        {
+          type: 'public-key',
+          alg:
+              -7,  // ECDSA with SHA-256 (See
+                   // https://www.iana.org/assignments/cose/cose.xhtml#algorithms)
+        },
+        {
+          type: 'public-key',
+          alg:
+              -257,  // RSA with SHA-256 (See
+                     // https://www.iana.org/assignments/cose/cose.xhtml#algorithms)
+        }
+      ]
     });
 
     try {
@@ -37,7 +55,8 @@ async function onBuyClicked(windowLocalStorageIdentifier) {
     await instrumentResponse.complete('success')
     console.log(instrumentResponse);
     info(windowLocalStorageIdentifier + ' payment response: ' +
-      objectToString(instrumentResponse));
+      objectToString(instrumentResponse) + '\n' + 'Extensions: ' +
+      extensionsOutputToString(instrumentResponse.details));
   } catch (err) {
     error(err);
   }
@@ -64,6 +83,18 @@ async function webAuthnGet(windowLocalStorageIdentifier) {
   } catch (err) {
     error(err);
   }
+}
+
+function extensionsOutputToString(credentialInfoAssertion) {
+  const clientExtensionResults =
+      credentialInfoAssertion.getClientExtensionResults();
+  if (clientExtensionResults.payment !== undefined &&
+      clientExtensionResults.payment.browserBoundSignature !== undefined) {
+      var browserBoundSignature = clientExtensionResults.payment.browserBoundSignature;
+      browserBoundSignature.signature =
+          arrayBufferToBase64(browserBoundSignature.signature);
+  }
+  return JSON.stringify(clientExtensionResults, /*replacer=*/ undefined, /*space=*/ 2);
 }
 
 if (PublicKeyCredential) {
