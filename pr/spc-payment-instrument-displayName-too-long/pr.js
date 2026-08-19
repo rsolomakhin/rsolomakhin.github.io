d32512ac2a6a200e32e93e9aac3689a19043676a
Stephen McGruer <smcgruer@google.com>
2026-08-18 21:43:58 -0400

    Add demo for too long displayName

---
+++ pr/spc-payment-instrument-displayName-too-long/pr.js
@@ -0,0 +1,137 @@
+/* exported createPaymentCredential */
+/* exported onBuyClicked */
+
+/**
+ * Creates a payment credential.
+ */
+async function createPaymentCredential(windowLocalStorageIdentifier) {
+  try {
+    const publicKeyCredential = await createCredential(/*setPaymentExtension=*/true);
+    console.log(publicKeyCredential);
+    window.localStorage.setItem(windowLocalStorageIdentifier,
+      arrayBufferToBase64(publicKeyCredential.rawId));
+    info(windowLocalStorageIdentifier + ' enrolled: ' + objectToString(
+      publicKeyCredential) + '\n' + 'Extensions: ' +
+      extensionsOutputToString(publicKeyCredential));
+    const verificationResult = await verifyBrowserBoundKey(
+      publicKeyCredential, [cose_key_type_ec2, cose_key_type_rsa]);
+    info(verificationResult);
+  } catch (err) {
+    error(err);
+  }
+}
+
+/**
+ * Launches payment request for SPC.
+ */
+async function onBuyClicked(windowLocalStorageIdentifier) {
+  try {
+    const request = await createSPCPaymentRequest({
+      credentialIds: [base64ToArray(
+          window.localStorage.getItem(windowLocalStorageIdentifier))],
+      instrument: {
+          displayName: 'This is a very long display name but it should still not be truncated and should be shown in its entirety',
+      },
+      paymentEntitiesLogos: [
+        {url: 'https://rsolomakhin.github.io/static/sync-network-logo.png', label: 'Sync Network'},
+        {url: 'https://rsolomakhin.github.io/static/troy-alt-logo.png', label: 'TroyBank'},
+      ],
+      // `browserBoundPubKeyCredParams` does not need to be set and will default
+      // to the same values listed here.
+      browserBoundPubKeyCredParams: [
+        {
+          type: 'public-key',
+          alg:
+              -7,  // ECDSA with SHA-256 (See
+                   // https://www.iana.org/assignments/cose/cose.xhtml#algorithms)
+        },
+        {
+          type: 'public-key',
+          alg:
+              -257,  // RSA with SHA-256 (See
+                     // https://www.iana.org/assignments/cose/cose.xhtml#algorithms)
+        }
+      ]
+    });
+
+    try {
+      const canMakePayment = await request.canMakePayment();
+      info(`canMakePayment result: ${canMakePayment}`);
+    } catch (err) {
+      error(`Error from canMakePayment: ${error.message}`);
+    }
+
+    const response = await request.show();
+    await response.complete('success')
+    console.log(response);
+    info(windowLocalStorageIdentifier + ' payment response: ' +
+      objectToString(response) + '\n' + 'Extensions: ' +
+      extensionsOutputToString(response.details));
+    const verificationResult = await verifyBrowserBoundKey(
+      response.details, [cose_key_type_ec2, cose_key_type_rsa]);
+    info(verificationResult);
+  } catch (err) {
+    error(err);
+  }
+}
+
+async function webAuthnGet(windowLocalStorageIdentifier) {
+  try {
+    const publicKey = {
+      challenge: new TextEncoder().encode('Authentication challenge'),
+      userVerification: 'required',
+      allowCredentials: [{
+        transports: ['internal'],
+        type: 'public-key',
+        id: base64ToArray(window.localStorage.getItem(
+          windowLocalStorageIdentifier)),
+      }, ],
+    };
+    const credentialInfoAssertion = await navigator.credentials.get({
+      publicKey
+    });
+    console.log(credentialInfoAssertion);
+    info('Successful login with ' + windowLocalStorageIdentifier + ': ' +
+      objectToString(credentialInfoAssertion));
+  } catch (err) {
+    error(err);
+  }
+}
+
+function extensionsOutputToString(credentialInfoAssertion) {
+  const clientExtensionResults =
+      credentialInfoAssertion.getClientExtensionResults();
+  if (clientExtensionResults.payment !== undefined &&
+      clientExtensionResults.payment.browserBoundSignature !== undefined) {
+      var browserBoundSignature = clientExtensionResults.payment.browserBoundSignature;
+      browserBoundSignature.signature =
+          arrayBufferToBase64(browserBoundSignature.signature);
+  }
+  return JSON.stringify(clientExtensionResults, /*replacer=*/ undefined, /*space=*/ 2);
+}
+
+if (PublicKeyCredential) {
+  if (PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable) {
+    PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable()
+      .then((available) => {
+        info(`isUserVerifyingPlatformAuthenticatorAvailable: ${available}`);
+      }).catch((error) => {
+        error(`Error when calling isUserVerifyingPlatformAuthenticatorAvailable: ${error.message}`);
+      });
+  } else {
+    error('PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable method not detected');
+  }
+} else {
+  error('PublicKeyCredential interface not detected');
+}
+
+if (PaymentRequest && PaymentRequest.securePaymentConfirmationAvailability) {
+  PaymentRequest.securePaymentConfirmationAvailability()
+    .then((available) => {
+      info(`PaymentRequest.securePaymentConfirmationAvailability: ${available}`);
+    }).catch((error) => {
+      error(`Error when calling PaymentRequest.securePaymentConfirmationAvailability: ${error.message}`);
+    });
+} else {
+  info('PaymentRequest.securePaymentConfirmationAvailability method not available');
+}
