6850587416b9a9ecfb88f4f627c510172d7e0d5d
Stephen McGruer <smcgruer@chromium.org>
2026-09-03 16:22:41 +0000

    Fixup

--- pr/spc-capabilities-in-3p-popup/pr.js
+++ pr/spc-capabilities-in-3p-popup/pr.js
@@ -1,31 +1,37 @@
 /* exported checkCapabilities */
 
+let checkCount = 0;
+
 /**
  * Calls static getSecurePaymentConfirmationCapabilities method on PaymentRequest
  * and displays the result visually.
  */
 async function checkCapabilities() {
+  checkCount++;
+  const now = new Date();
+  const timeString = now.toLocaleTimeString() + '.' + String(now.getMilliseconds()).padStart(3, '0');
+  const runHeader = `Run #${checkCount} at ${timeString}`;
+
   clearAllMessages();
 
   try {
     if (typeof PaymentRequest === 'undefined') {
-      error('PaymentRequest API is not supported.');
+      error(`[${runHeader}] PaymentRequest API is not supported.`);
       return;
     }
 
     if (!PaymentRequest.getSecurePaymentConfirmationCapabilities) {
-      error('PaymentRequest.getSecurePaymentConfirmationCapabilities API is not supported.');
+      error(`[${runHeader}] PaymentRequest.getSecurePaymentConfirmationCapabilities API is not supported.`);
       return;
     }
 
-    info('Calling PaymentRequest.getSecurePaymentConfirmationCapabilities()...');
     const capabilities = await PaymentRequest.getSecurePaymentConfirmationCapabilities();
 
     clearAllMessages();
-    info(JSON.stringify(capabilities, undefined, 2));
+    info(`Capabilities [${runHeader}]:\n` + JSON.stringify(capabilities, undefined, 2));
   } catch (err) {
     clearAllMessages();
-    error(err && err.message ? err.message : String(err));
+    error(`Error [${runHeader}]: ` + (err && err.message ? err.message : String(err)));
   }
 }
 
